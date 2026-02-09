e0d5fcd62743b4d3ee082d769edacf30c46fd49a
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 06:20:53 +0800

    Batch add 1 solutions: 3425.longest-special...

---
+++ 3425.longest-special-path.cpp
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=3425 lang=cpp
+#
+# [3425] Longest Special Path
+#
+# @lc code=start
+class Solution {
+public:
+    vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
+        
+    }
+};
+# @lc code=end
