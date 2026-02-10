3ed05a0c02f0d882ff7dd573e8b442fd87b542a0
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 02:44:27 +0800

    Batch add 1 solutions: 3569.maximize-count-...

--- 3569.maximize-count-of-distinct-primes-after-split.cpp
+++ 3569.maximize-count-of-distinct-primes-after-split.cpp
@@ -3,58 +3,129 @@
 #
 # [3569] Maximize Count of Distinct Primes After Split
 #
+
 # @lc code=start
 class Solution {
 public:
     vector<int> maximumCount(vector<int>& nums, vector<vector<int>>& queries) {
-        // Precompute primes using Sieve of Eratosthenes
-        int maxVal = 100001;
-        vector<bool> isPrime(maxVal, true);
-        isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i * i < maxVal; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < maxVal; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-        
-        vector<int> result;
         int n = nums.size();
-        
-        for (auto& query : queries) {
-            int idx = query[0];
-            int val = query[1];
-            nums[idx] = val;
-            
-            int maxCount = 0;
-            
-            // Try all possible split positions k (1 <= k < n)
-            for (int k = 1; k < n; k++) {
-                unordered_set<int> leftPrimes, rightPrimes;
-                
-                // Count distinct primes in left part [0..k-1]
-                for (int i = 0; i < k; i++) {
-                    if (isPrime[nums[i]]) {
-                        leftPrimes.insert(nums[i]);
-                    }
+        const int MAXV = 100010;
+        vector<bool> is_prime(MAXV, true);
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i < MAXV; ++i) {
+            if (is_prime[i]) {
+                for (int j = i * i; j < MAXV; j += i) {
+                    is_prime[j] = false;
                 }
-                
-                // Count distinct primes in right part [k..n-1]
-                for (int i = k; i < n; i++) {
-                    if (isPrime[nums[i]]) {
-                        rightPrimes.insert(nums[i]);
-                    }
-                }
-                
-                int count = leftPrimes.size() + rightPrimes.size();
-                maxCount = max(maxCount, count);
             }
-            
-            result.push_back(maxCount);
         }
-        
-        return result;
+        vector<set<int>> pos(MAXV);
+        int D = 0;
+        for (int i = 0; i < n; ++i) {
+            int v = nums[i];
+            if (v < MAXV && is_prime[v]) {
+                auto& s = pos[v];
+                size_t sz = s.size();
+                s.insert(i);
+                if (sz == 0) ++D;
+            }
+        }
+        struct SegTree {
+            int nn;
+            vector<long long> tree, lazy;
+            SegTree(int _n) : nn(_n), tree(4 * _n + 10, 0LL), lazy(4 * _n + 10, 0LL) {}
+            void push(int node, int l, int r) {
+                if (lazy[node] != 0) {
+                    tree[node] += lazy[node];
+                    if (l != r) {
+                        lazy[2 * node] += lazy[node];
+                        lazy[2 * node + 1] += lazy[node];
+                    }
+                    lazy[node] = 0;
+                }
+            }
+            void range_add(int ql, int qr, long long val, int node, int l, int r) {
+                push(node, l, r);
+                if (ql > r || qr < l) return;
+                if (ql <= l && r <= qr) {
+                    lazy[node] += val;
+                    push(node, l, r);
+                    return;
+                }
+                int m = (l + r) / 2;
+                range_add(ql, qr, val, 2 * node, l, m);
+                range_add(ql, qr, val, 2 * node + 1, m + 1, r);
+                tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+            }
+            void range_add(int ql, int qr, long long val) {
+                if (ql > qr) return;
+                range_add(ql, qr, val, 1, 1, nn);
+            }
+            long long query_max() {
+                push(1, 1, nn);
+                return tree[1];
+            }
+        };
+        SegTree st(n - 1);
+        for (int p = 2; p < MAXV; ++p) {
+            auto& s = pos[p];
+            if (s.size() >= 2) {
+                int mn = *s.begin();
+                int mx = *s.rbegin();
+                st.range_add(mn + 1, mx, 1LL);
+            }
+        }
+        vector<int> ans;
+        for (auto& q : queries) {
+            int idx = q[0], nval = q[1];
+            int oval = nums[idx];
+            nums[idx] = nval;
+            // handle old
+            if (oval < MAXV && is_prime[oval]) {
+                int p = oval;
+                auto& s = pos[p];
+                size_t osz = s.size();
+                int omn = osz ? *s.begin() : -1;
+                int omx = osz ? *s.rbegin() : -1;
+                bool wcontrib = (osz >= 2);
+                s.erase(idx);
+                size_t nsz = s.size();
+                int nmn = (nsz >= 2 ? *s.begin() : -1);
+                int nmx = (nsz >= 2 ? *s.rbegin() : -1);
+                bool ncontrib = (nsz >= 2);
+                if (wcontrib) {
+                    st.range_add(omn + 1, omx, -1LL);
+                }
+                if (ncontrib) {
+                    st.range_add(nmn + 1, nmx, 1LL);
+                }
+                if (osz >= 1 && nsz == 0) --D;
+            }
+            // handle new
+            if (nval < MAXV && is_prime[nval]) {
+                int p = nval;
+                auto& s = pos[p];
+                size_t osz = s.size();
+                int omn = osz ? *s.begin() : -1;
+                int omx = osz ? *s.rbegin() : -1;
+                bool wcontrib = (osz >= 2);
+                s.insert(idx);
+                size_t nsz = s.size();
+                int nmn = *s.begin();
+                int nmx = *s.rbegin();
+                bool ncontrib = (nsz >= 2);
+                if (wcontrib) {
+                    st.range_add(omn + 1, omx, -1LL);
+                }
+                if (ncontrib) {
+                    st.range_add(nmn + 1, nmx, 1LL);
+                }
+                if (osz == 0) ++D;
+            }
+            long long mstrad = st.query_max();
+            ans.push_back(D + (int)mstrad);
+        }
+        return ans;
     }
 };
 # @lc code=end
