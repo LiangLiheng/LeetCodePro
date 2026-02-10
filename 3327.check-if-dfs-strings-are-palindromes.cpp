d1749b41fd7102d3c1bd40fc63ad400faf78c97e
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 22:32:21 +0800

    Batch add 1 solutions: 3327.check-if-dfs-st...

--- 3327.check-if-dfs-strings-are-palindromes.cpp
+++ 3327.check-if-dfs-strings-are-palindromes.cpp
@@ -3,78 +3,61 @@
 #
 # [3327] Check if DFS Strings Are Palindromes
 #
+
 # @lc code=start
 class Solution {
 public:
     vector<bool> findAnswer(vector<int>& parent, string s) {
-        int n = parent.size();
-        vector<vector<int>> children(n);
-        
-        // Build adjacency list
-        for (int i = 1; i < n; i++) {
-            children[parent[i]].push_back(i);
+        int n = s.size();
+        vector<vector<int>> adj(n);
+        for (int i = 1; i < n; ++i) {
+            adj[parent[i]].push_back(i);
         }
-        
-        // Sort children for each node
-        for (int i = 0; i < n; i++) {
-            sort(children[i].begin(), children[i].end());
+        for (auto& ch : adj) {
+            sort(ch.begin(), ch.end());
         }
-        
-        // Build DFS string and record ranges
-        string dfsStr;
-        vector<int> start(n), end(n);
-        
-        function<void(int)> dfs = [&](int x) {
-            start[x] = dfsStr.size();
-            for (int child : children[x]) {
-                dfs(child);
+        const long long MOD1 = 1000000007LL;
+        const long long MOD2 = 1000000009LL;
+        const long long B1 = 31LL;
+        const long long B2 = 37LL;
+        vector<long long> p1(n + 1, 1LL);
+        vector<long long> p2(n + 1, 1LL);
+        for (int i = 1; i <= n; ++i) {
+            p1[i] = p1[i - 1] * B1 % MOD1;
+            p2[i] = p2[i - 1] * B2 % MOD2;
+        }
+        vector<long long> hf1(n), hf2(n), hr1(n), hr2(n);
+        vector<int> len(n);
+        std::function<void(int)> dfs = [&](int u) {
+            len[u] = 1;
+            long long f1 = 0, f2 = 0;
+            long long r1 = (s[u] - 'a' + 1) % MOD1;
+            long long r2 = (s[u] - 'a' + 1) % MOD2;
+            for (int v : adj[u]) {
+                dfs(v);
+                len[u] += len[v];
+                f1 = (f1 * p1[len[v]] % MOD1 + hf1[v]) % MOD1;
+                f2 = (f2 * p2[len[v]] % MOD2 + hf2[v]) % MOD2;
             }
-            dfsStr += s[x];
-            end[x] = dfsStr.size() - 1;
+            long long ch = s[u] - 'a' + 1;
+            f1 = (f1 * p1[1] % MOD1 + ch) % MOD1;
+            f2 = (f2 * p2[1] % MOD2 + ch) % MOD2;
+            for (int j = adj[u].size() - 1; j >= 0; --j) {
+                int v = adj[u][j];
+                r1 = (r1 * p1[len[v]] % MOD1 + hr1[v]) % MOD1;
+                r2 = (r2 * p2[len[v]] % MOD2 + hr2[v]) % MOD2;
+            }
+            hf1[u] = f1;
+            hf2[u] = f2;
+            hr1[u] = r1;
+            hr2[u] = r2;
         };
-        
         dfs(0);
-        
-        int m = dfsStr.size();
-        const long long MOD = 1e9 + 7;
-        const long long BASE = 31;
-        
-        // Precompute powers of BASE
-        vector<long long> powBase(m + 1);
-        powBase[0] = 1;
-        for (int i = 1; i <= m; i++) {
-            powBase[i] = (powBase[i-1] * BASE) % MOD;
+        vector<bool> ans(n);
+        for (int i = 0; i < n; ++i) {
+            ans[i] = (hf1[i] == hr1[i] && hf2[i] == hr2[i]);
         }
-        
-        // Prefix hash (forward)
-        vector<long long> prefixHash(m + 1, 0);
-        for (int i = 0; i < m; i++) {
-            prefixHash[i+1] = (prefixHash[i] * BASE + (dfsStr[i] - 'a' + 1)) % MOD;
-        }
-        
-        // Suffix hash (backward)
-        vector<long long> suffixHash(m + 1, 0);
-        for (int i = m - 1; i >= 0; i--) {
-            suffixHash[m-i] = (suffixHash[m-i-1] * BASE + (dfsStr[i] - 'a' + 1)) % MOD;
-        }
-        
-        // Helper to get forward hash of substring [l, r]
-        auto getForwardHash = [&](int l, int r) -> long long {
-            return (prefixHash[r+1] - prefixHash[l] * powBase[r-l+1] % MOD + MOD) % MOD;
-        };
-        
-        // Helper to get backward hash of substring [l, r]
-        auto getBackwardHash = [&](int l, int r) -> long long {
-            return (suffixHash[m-l] - suffixHash[m-r-1] * powBase[r-l+1] % MOD + MOD) % MOD;
-        };
-        
-        // Check palindrome for each node
-        vector<bool> answer(n);
-        for (int i = 0; i < n; i++) {
-            answer[i] = (getForwardHash(start[i], end[i]) == getBackwardHash(start[i], end[i]));
-        }
-        
-        return answer;
+        return ans;
     }
 };
 # @lc code=end
