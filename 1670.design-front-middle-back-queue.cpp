b98519656fc57f088c8c284696d27c4a8d1d68f9
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 17:12:25 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.cpp
+++ 1670.design-front-middle-back-queue.cpp
@@ -9,20 +9,17 @@ private:
     deque<int> left, right;
     
     void balance() {
-        while (left.size() > right.size() + 1) {
+        if (left.size() > right.size()) {
             right.push_front(left.back());
             left.pop_back();
-        }
-        while (left.size() < right.size()) {
+        } else if (right.size() > left.size() + 1) {
             left.push_back(right.front());
             right.pop_front();
         }
     }
     
 public:
-    FrontMiddleBackQueue() {
-        
-    }
+    FrontMiddleBackQueue() {}
     
     void pushFront(int val) {
         left.push_front(val);
@@ -30,11 +27,12 @@ public:
     }
     
     void pushMiddle(int val) {
-        if (left.size() > right.size()) {
-            right.push_front(left.back());
-            left.pop_back();
+        if (left.size() == right.size()) {
+            left.push_back(val);
+        } else {
+            right.push_front(val);
         }
-        left.push_back(val);
+        balance();
     }
     
     void pushBack(int val) {
@@ -45,12 +43,12 @@ public:
     int popFront() {
         if (left.empty() && right.empty()) return -1;
         int val;
-        if (left.empty()) {
-            val = right.front();
-            right.pop_front();
-        } else {
+        if (!left.empty()) {
             val = left.front();
             left.pop_front();
+        } else {
+            val = right.front();
+            right.pop_front();
         }
         balance();
         return val;
@@ -58,8 +56,14 @@ public:
     
     int popMiddle() {
         if (left.empty() && right.empty()) return -1;
-        int val = left.back();
-        left.pop_back();
+        int val;
+        if (left.size() == right.size()) {
+            val = left.back();
+            left.pop_back();
+        } else {
+            val = right.front();
+            right.pop_front();
+        }
         balance();
         return val;
     }
@@ -67,12 +71,12 @@ public:
     int popBack() {
         if (left.empty() && right.empty()) return -1;
         int val;
-        if (right.empty()) {
-            val = left.back();
-            left.pop_back();
-        } else {
+        if (!right.empty()) {
             val = right.back();
             right.pop_back();
+        } else {
+            val = left.back();
+            left.pop_back();
         }
         balance();
         return val;
