4c4e058ce88c9de059e1b1396e8327ed3c8b1834
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 22:28:57 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.cpp
+++ 2816.double-a-number-represented-as-a-linked-list.cpp
@@ -3,40 +3,63 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        int carry = doubleHelper(head);
-        if (carry > 0) {
-            ListNode* newHead = new ListNode(carry);
-            newHead->next = head;
-            return newHead;
+        // Reverse the list to make LSD first
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
-        return head;
-    }
-    
-private:
-    int doubleHelper(ListNode* node) {
-        if (node == nullptr) return 0;
-        
-        // Get carry from next node
-        int carry = doubleHelper(node->next);
-        
-        // Double current value and add carry
-        int doubled = node->val * 2 + carry;
-        node->val = doubled % 10;
-        return doubled / 10;
+        ListNode* newHead = prev;
+
+        // Process doubling with carry, track tail
+        ListNode* iter = newHead;
+        int carry = 0;
+        ListNode* listTail = nullptr;
+        while (iter != nullptr) {
+            int temp = iter->val * 2 + carry;
+            iter->val = temp % 10;
+            carry = temp / 10;
+            listTail = iter;
+            iter = iter->next;
+        }
+
+        // Append new node(s) if carry remains
+        while (carry != 0) {
+            ListNode* newNode = new ListNode(carry % 10);
+            listTail->next = newNode;
+            listTail = newNode;
+            carry /= 10;
+        }
+
+        // Reverse back to MSD first
+        prev = nullptr;
+        curr = newHead;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
     }
 };
 # @lc code=end
