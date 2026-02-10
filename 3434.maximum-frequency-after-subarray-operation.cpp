b684c8e67b2e6eac2e0fbe113e5c76ff1edad24d
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 23:59:29 +0800

    Batch add 1 solutions: 3434.maximum-frequen...

--- 3434.maximum-frequency-after-subarray-operation.cpp
+++ 3434.maximum-frequency-after-subarray-operation.cpp
@@ -3,56 +3,30 @@
 #
 # [3434] Maximum Frequency After Subarray Operation
 #
+
 # @lc code=start
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        // Count initial frequency of k
-        int initialCount = 0;
+        int total = 0;
         for (int num : nums) {
-            if (num == k) initialCount++;
+            if (num == k) total++;
         }
-        
-        int maxResult = initialCount;
-        
-        // Get unique values to try converting
-        unordered_set<int> uniqueVals(nums.begin(), nums.end());
-        
-        // Try converting each unique value v to k
-        for (int v : uniqueVals) {
-            if (v == k) continue;  // Skip if already k
-            
-            // Create score array:
-            // +1 for elements equal to v (can be converted to k)
-            // -1 for elements equal to k (will be changed away from k)
-            // 0 for other elements
-            vector<int> score(n);
-            for (int i = 0; i < n; i++) {
-                if (nums[i] == v) {
-                    score[i] = 1;
-                } else if (nums[i] == k) {
-                    score[i] = -1;
-                } else {
-                    score[i] = 0;
-                }
+        int ans = total;
+        for (int v = 1; v <= 50; v++) {
+            int cur = 0;
+            int maxs = 0;
+            for (int num : nums) {
+                int d = 0;
+                if (num == v) d++;
+                if (num == k) d--;
+                cur = std::max(d, cur + d);
+                maxs = std::max(maxs, cur);
             }
-            
-            // Use Kadane's algorithm to find maximum subarray sum
-            // This represents the best gain from selecting a subarray
-            int maxSum = 0;
-            int currentSum = 0;
-            for (int s : score) {
-                currentSum = max(0, currentSum + s);
-                maxSum = max(maxSum, currentSum);
-            }
-            
-            // Total frequency = initial count + gain from best subarray
-            maxResult = max(maxResult, initialCount + maxSum);
+            ans = std::max(ans, total + maxs);
         }
-        
-        return maxResult;
+        return ans;
     }
 };
 # @lc code=end
