a902483334964c527439627913cf031d0b817c6a
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 15:52:10 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.cpp
+++ 3721.longest-balanced-subarray-ii.cpp
@@ -10,19 +10,26 @@ public:
         int n = nums.size();
         int maxLen = 0;
         
+        // Try all possible subarrays
         for (int i = 0; i < n; i++) {
-            unordered_set<int> evens, odds;
+            unordered_set<int> evens;
+            unordered_set<int> odds;
+            
             for (int j = i; j < n; j++) {
+                // Add current number to appropriate set
                 if (nums[j] % 2 == 0) {
                     evens.insert(nums[j]);
                 } else {
                     odds.insert(nums[j]);
                 }
+                
+                // Check if balanced
                 if (evens.size() == odds.size()) {
                     maxLen = max(maxLen, j - i + 1);
                 }
             }
         }
+        
         return maxLen;
     }
 };
