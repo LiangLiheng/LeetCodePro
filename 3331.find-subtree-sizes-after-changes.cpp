29708cbc2ed19c8ee2060309dfdd80e01b53db81
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 23:08:37 +0800

    Batch add 1 solutions: 3331.find-subtree-si...

--- 3331.find-subtree-sizes-after-changes.cpp
+++ 3331.find-subtree-sizes-after-changes.cpp
@@ -8,43 +8,44 @@
 class Solution {
 public:
     vector<int> findSubtreeSizes(vector<int>& parent, string s) {
-        int n = parent.size();
+        int n = s.size();
         vector<vector<int>> adj(n);
         for (int i = 1; i < n; ++i) {
             adj[parent[i]].push_back(i);
         }
-        vector<int> new_parent = parent;
-        vector<int> last_ancestor(26, -1);
-        auto dfs_modify = [&](auto&& self, int u, int p) -> void {
-            int idx = s[u] - 'a';
-            if (last_ancestor[idx] != -1) {
-                new_parent[u] = last_ancestor[idx];
-            }
-            int prev = last_ancestor[idx];
-            last_ancestor[idx] = u;
-            for (int v : adj[u]) {
-                if (v != p) {
-                    self(self, v, u);
+        vector<int> np = parent;
+        vector<int> last(26, -1);
+        auto dfs_update = [&](auto&& self, int u) -> void {
+            if (u != 0) {
+                int c = s[u] - 'a';
+                if (last[c] != -1) {
+                    np[u] = last[c];
                 }
             }
-            last_ancestor[idx] = prev;
-        };
-        dfs_modify(dfs_modify, 0, -1);
-        vector<vector<int>> new_adj(n);
-        for (int i = 1; i < n; ++i) {
-            new_adj[new_parent[i]].push_back(i);
-        }
-        vector<int> subtree_sizes(n);
-        auto dfs_size = [&](auto&& self, int u) -> int {
-            int size = 1;
-            for (int v : new_adj[u]) {
-                size += self(self, v);
+            int c = s[u] - 'a';
+            int prv = last[c];
+            last[c] = u;
+            for (int v : adj[u]) {
+                self(self, v);
             }
-            subtree_sizes[u] = size;
-            return size;
+            last[c] = prv;
+        };
+        dfs_update(dfs_update, 0);
+        vector<vector<int>> nadj(n);
+        for (int i = 1; i < n; ++i) {
+            nadj[np[i]].push_back(i);
+        }
+        vector<int> subtree(n, 1);
+        auto dfs_size = [&](auto&& self, int u) -> int {
+            int sz = 1;
+            for (int v : nadj[u]) {
+                sz += self(self, v);
+            }
+            subtree[u] = sz;
+            return sz;
         };
         dfs_size(dfs_size, 0);
-        return subtree_sizes;
+        return subtree;
     }
 };
 # @lc code=end
