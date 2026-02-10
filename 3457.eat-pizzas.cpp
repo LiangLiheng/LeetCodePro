0a0a996d4eda5bab192ea9c17535d3c3132e2b34
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 18:13:54 +0800

    Batch add 1 solutions: 3457.eat-pizzas.cpp...

--- 3457.eat-pizzas.cpp
+++ 3457.eat-pizzas.cpp
@@ -7,29 +7,29 @@
 class Solution {
 public:
     long long maxWeight(vector<int>& pizzas) {
-        int n = pizzas.size();
-        
-        // Sort pizzas in descending order
         sort(pizzas.begin(), pizzas.end(), greater<int>());
         
-        int total_days = n / 4;
-        int k1 = (total_days + 1) / 2;  // Number of odd days
-        int k2 = total_days / 2;         // Number of even days
+        int n = pizzas.size();
+        int days = n / 4;
+        long long totalWeight = 0;
+        int left = 0, right = n - 1;
         
-        long long result = 0;
-        
-        // Gain from odd days: sum of largest k1 pizzas (used as max in odd-day groups)
-        for (int i = 0; i < k1; i++) {
-            result += pizzas[i];
+        for (int day = 1; day <= days; day++) {
+            if (day % 2 == 1) {  // Odd day
+                // Take 1 from left (largest) and 3 from right (smallest)
+                totalWeight += pizzas[left];
+                left++;
+                right -= 3;
+            } else {  // Even day
+                // Take 2 from left and 2 from right
+                // Gain the second largest, which is pizzas[left + 1]
+                totalWeight += pizzas[left + 1];
+                left += 2;
+                right -= 2;
+            }
         }
         
-        // Gain from even days: sum of pizzas used as second-max in even-day groups
-        // These are from index k1+k2 to k1+2*k2-1
-        for (int i = k1 + k2; i < k1 + 2 * k2; i++) {
-            result += pizzas[i];
-        }
-        
-        return result;
+        return totalWeight;
     }
 };
 # @lc code=end
