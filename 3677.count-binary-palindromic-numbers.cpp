aa012f622fe6c638608e9ed4df544807f4742337
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 15:23:57 +0800

    Batch add 1 solutions: 3677.count-binary-pa...

--- 3677.count-binary-palindromic-numbers.cpp
+++ 3677.count-binary-palindromic-numbers.cpp
@@ -3,68 +3,89 @@
 #
 # [3677] Count Binary Palindromic Numbers
 #
+
 # @lc code=start
 class Solution {
 public:
     int countBinaryPalindromes(long long n) {
         if (n == 0) return 1;
         
-        // Get binary representation of n
-        string binary = "";
+        // Find bit length of n
+        int maxLen = 0;
         long long temp = n;
         while (temp > 0) {
-            binary = (char)('0' + (temp % 2)) + binary;
-            temp /= 2;
+            maxLen++;
+            temp >>= 1;
         }
         
-        int len = binary.length();
-        int count = 1; // Count 0
+        int count = 0;
         
-        // Count palindromes with length < len
-        for (int l = 1; l < len; l++) {
-            if (l == 1) {
-                count += 1; // Only "1"
-            } else if (l % 2 == 1) {
-                // Odd length: 2^((l-1)/2) palindromes
-                count += (1 << ((l - 1) / 2));
-            } else {
-                // Even length: 2^(l/2 - 1) palindromes
-                count += (1 << (l / 2 - 1));
+        // Count palindromes with length < maxLen
+        if (maxLen > 1) {
+            count += 2; // Length 1: "0" and "1"
+            
+            // For each length from 2 to maxLen-1
+            for (int len = 2; len < maxLen; len++) {
+                if (len % 2 == 0) {
+                    // Even length: 2^(len/2-1) palindromes
+                    count += (1 << (len / 2 - 1));
+                } else {
+                    // Odd length: 2^((len-1)/2) palindromes
+                    count += (1 << ((len - 1) / 2));
+                }
             }
         }
         
-        // Count palindromes with length = len and value <= n
-        int halfLen = (len + 1) / 2;
-        
-        for (long long i = 0; i < (1LL << (halfLen - 1)); i++) {
-            // Construct the first half
-            string palin = "1";
-            
-            // Add the remaining bits of the first half
-            for (int j = halfLen - 2; j >= 0; j--) {
-                palin += ((i >> j) & 1) ? '1' : '0';
-            }
-            
-            // Mirror to create full palindrome
-            if (len % 2 == 1) {
-                // Odd length: mirror all except the last bit (middle bit)
-                for (int j = halfLen - 2; j >= 0; j--) {
-                    palin += palin[j];
-                }
-            } else {
-                // Even length: mirror all bits
-                for (int j = halfLen - 1; j >= 0; j--) {
-                    palin += palin[j];
-                }
-            }
-            
-            // Check if palindrome <= n
-            if (palin <= binary) {
-                count++;
-            }
+        // Count palindromes with length == maxLen and value <= n
+        if (maxLen == 1) {
+            count = n + 1; // Both 0 and 1 (or just 0 if n=0, handled above)
+        } else {
+            count += countPalindromesOfLength(maxLen, n);
         }
         
         return count;
+    }
+    
+private:
+    // Count palindromes of specific length that are <= maxVal
+    int countPalindromesOfLength(int len, long long maxVal) {
+        bool isOdd = (len % 2 == 1);
+        int halfLen = isOdd ? (len + 1) / 2 : len / 2;
+        
+        long long minHalf = 1LL << (halfLen - 1); // Minimum half (first bit is 1)
+        long long maxHalf = (1LL << halfLen) - 1; // Maximum half
+        
+        // Binary search for the maximum valid half
+        long long left = minHalf, right = maxHalf;
+        long long result = minHalf - 1;
+        
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long palindrome = createPalindrome(mid, isOdd);
+            
+            if (palindrome <= maxVal) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        if (result < minHalf) return 0;
+        return (int)(result - minHalf + 1);
+    }
+    
+    // Create palindrome from first half
+    long long createPalindrome(long long half, bool isOdd) {
+        long long result = half;
+        if (isOdd) half >>= 1; // Skip middle bit for odd length
+        
+        // Mirror the remaining bits
+        while (half > 0) {
+            result = (result << 1) | (half & 1);
+            half >>= 1;
+        }
+        return result;
     }
 };
 # @lc code=end
