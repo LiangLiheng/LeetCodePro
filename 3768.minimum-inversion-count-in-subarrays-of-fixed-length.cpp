1d97f538c3c8d438c866ef6e38391c317bcf4612
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 16:41:26 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
@@ -5,51 +5,73 @@
 #
 # @lc code=start
 class Solution {
+private:
+    long long mergeCount(vector<int>& arr, int left, int mid, int right) {
+        vector<int> temp;
+        int i = left, j = mid + 1;
+        long long count = 0;
+        
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp.push_back(arr[i++]);
+            } else {
+                temp.push_back(arr[j++]);
+                count += (mid - i + 1);
+            }
+        }
+        
+        while (i <= mid) temp.push_back(arr[i++]);
+        while (j <= right) temp.push_back(arr[j++]);
+        
+        for (int i = 0; i < temp.size(); i++) {
+            arr[left + i] = temp[i];
+        }
+        
+        return count;
+    }
+    
+    long long mergeSortCount(vector<int>& arr, int left, int right) {
+        long long count = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count += mergeSortCount(arr, left, mid);
+            count += mergeSortCount(arr, mid + 1, right);
+            count += mergeCount(arr, left, mid, right);
+        }
+        return count;
+    }
+    
 public:
     long long minInversionCount(vector<int>& nums, int k) {
         int n = nums.size();
         if (k == 1) return 0;
         
-        // Count inversions in first window [0, k-1]
-        long long currentInversions = 0;
-        for (int i = 0; i < k; i++) {
-            for (int j = i + 1; j < k; j++) {
-                if (nums[i] > nums[j]) {
-                    currentInversions++;
-                }
-            }
-        }
+        // Calculate inversions for first window using merge sort
+        vector<int> temp(nums.begin(), nums.begin() + k);
+        long long count = mergeSortCount(temp, 0, k - 1);
+        long long minCount = count;
         
-        long long minInversions = currentInversions;
-        
-        // Build multiset for first window
+        // Sliding window with multiset
         multiset<int> window;
         for (int i = 0; i < k; i++) {
             window.insert(nums[i]);
         }
         
-        // Slide the window from position 1 to n-k
-        for (int i = 1; i <= n - k; i++) {
-            int removed = nums[i - 1];
-            int added = nums[i + k - 1];
+        for (int i = k; i < n; i++) {
+            // Remove nums[i-k]
+            int removed = nums[i - k];
+            count -= distance(window.begin(), window.lower_bound(removed));
+            window.erase(window.find(removed));
             
-            // Remove nums[i-1] from window
-            // Count how many elements are smaller (these were inversions)
-            auto it = window.find(removed);
-            long long smallerCount = distance(window.begin(), it);
-            currentInversions -= smallerCount;
-            window.erase(it);
-            
-            // Add nums[i+k-1] to window
-            // Count how many elements are larger (these create new inversions)
-            long long largerCount = distance(window.upper_bound(added), window.end());
-            currentInversions += largerCount;
+            // Add nums[i]
+            int added = nums[i];
+            count += distance(window.upper_bound(added), window.end());
             window.insert(added);
             
-            minInversions = min(minInversions, currentInversions);
+            minCount = min(minCount, count);
         }
         
-        return minInversions;
+        return minCount;
     }
 };
 # @lc code=end
