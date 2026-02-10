e0e1158459ad85cad50c09008ca6870a82cfb2a8
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 17:29:28 +0800

    Batch add 1 solutions: 3331.find-subtree-si...

--- 3331.find-subtree-sizes-after-changes.cpp
+++ 3331.find-subtree-sizes-after-changes.cpp
@@ -1,20 +1,23 @@
-//
-// @lc app=leetcode id=3331 lang=cpp
-//
-// [3331] Find Subtree Sizes After Changes
-//
-// @lc code=start
+#
+# @lc app=leetcode id=3331 lang=cpp
+#
+# [3331] Find Subtree Sizes After Changes
+#
+# @lc code=start
 class Solution {
 public:
     vector<int> findSubtreeSizes(vector<int>& parent, string s) {
         int n = parent.size();
         vector<int> newParent = parent;
         
-        // Find new parents for each node
+        // For each node from 1 to n-1, find closest ancestor with same character
         for (int i = 1; i < n; i++) {
+            char c = s[i];
             int curr = parent[i];
+            
+            // Travel up the tree to find closest ancestor with same character
             while (curr != -1) {
-                if (s[curr] == s[i]) {
+                if (s[curr] == c) {
                     newParent[i] = curr;
                     break;
                 }
@@ -22,27 +25,24 @@ public:
             }
         }
         
-        // Build adjacency list for new tree
+        // Build new tree structure
         vector<vector<int>> children(n);
         for (int i = 1; i < n; i++) {
             children[newParent[i]].push_back(i);
         }
         
         // Calculate subtree sizes using DFS
-        vector<int> result(n);
-        dfs(0, children, result);
+        vector<int> answer(n);
+        function<int(int)> dfs = [&](int node) -> int {
+            int size = 1;
+            for (int child : children[node]) {
+                size += dfs(child);
+            }
+            return answer[node] = size;
+        };
         
-        return result;
-    }
-    
-private:
-    int dfs(int node, vector<vector<int>>& children, vector<int>& result) {
-        int size = 1;
-        for (int child : children[node]) {
-            size += dfs(child, children, result);
-        }
-        result[node] = size;
-        return size;
+        dfs(0);
+        return answer;
     }
 };
-// @lc code=end
+# @lc code=end
