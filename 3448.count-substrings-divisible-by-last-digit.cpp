041276083b013687df10aafc28ea4ba6c03017fc
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 00:17:50 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.cpp
+++ 3448.count-substrings-divisible-by-last-digit.cpp
@@ -3,29 +3,46 @@
 #
 # [3448] Count Substrings Divisible By Last Digit
 #
+
 # @lc code=start
 class Solution {
 public:
     long long countSubstrings(string s) {
         int n = s.length();
-        long long count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int d = s[i] - '0';
-            if (d == 0) continue; // Skip zeros (non-zero last digit required)
-            
-            long long mod_value = 0;
-            long long power_of_10 = 1; // 10^(i-j) mod d
-            
-            for (int j = i; j >= 0; j--) {
-                int digit = s[j] - '0';
-                mod_value = ((long long)digit * power_of_10 % d + mod_value) % d;
-                if (mod_value == 0) count++;
-                power_of_10 = (power_of_10 * 10) % d;
+        long long total = 0;
+        for(int d = 1; d <= 9; d++) {
+            long long fr[10][10] = {0};
+            long long local_ans = 0;
+            for(int i = n - 1; i >= 0; i--) {
+                long long new_fr[10][10] = {0};
+                for(int md = 0; md < d; md++) {
+                    for(int pw = 0; pw < d; pw++) {
+                        long long c = fr[md][pw];
+                        if(c == 0) continue;
+                        int dig = s[i] - '0';
+                        int nmd = (int)(((long long)dig * pw % d + md) % d);
+                        int npw = (int)(((long long)pw * 10) % d);
+                        if(nmd == 0) {
+                            local_ans += c;
+                        }
+                        new_fr[nmd][npw] += c;
+                    }
+                }
+                for(int md = 0; md < d; md++) {
+                    for(int pw = 0; pw < d; pw++) {
+                        fr[md][pw] = new_fr[md][pw];
+                    }
+                }
+                int digi = s[i] - '0';
+                if(digi == d) {
+                    local_ans += 1;
+                    int ipw = 10 % d;
+                    fr[0][ipw] += 1;
+                }
             }
+            total += local_ans;
         }
-        
-        return count;
+        return total;
     }
 };
 # @lc code=end
