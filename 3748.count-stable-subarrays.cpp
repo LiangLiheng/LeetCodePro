52a310eef28abebb8d05a62716c5c6eadada5a65
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 04:22:07 +0800

    Batch add 1 solutions: 3748.count-stable-su...

--- 3748.count-stable-subarrays.cpp
+++ 3748.count-stable-subarrays.cpp
@@ -3,34 +3,63 @@
 #
 # [3748] Count Stable Subarrays
 #
+
 # @lc code=start
 class Solution {
 public:
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<long long> result;
-        
-        for (auto& query : queries) {
-            int l = query[0];
-            int r = query[1];
-            long long count = 0;
-            
-            int i = l;
-            while (i <= r) {
-                int j = i;
-                // Extend j to find maximal non-decreasing sequence
-                while (j < r && nums[j] <= nums[j + 1]) {
-                    j++;
-                }
-                // Count subarrays in this non-decreasing sequence
-                long long len = j - i + 1;
-                count += len * (len + 1) / 2;
-                i = j + 1;
+        int n = nums.size();
+        if (n == 0) return {};
+        vector<int> run_start_pos(n), run_end_pos(n), run_id(n);
+        vector<long long> full_contrib;
+        int rid = 0;
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j + 1 < n && nums[j] <= nums[j + 1]) {
+                ++j;
             }
-            
-            result.push_back(count);
+            long long len = static_cast<long long>(j - i + 1);
+            full_contrib.push_back(len * (len + 1) / 2);
+            for (int k = i; k <= j; ++k) {
+                run_start_pos[k] = i;
+                run_end_pos[k] = j;
+                run_id[k] = rid;
+            }
+            ++rid;
+            i = j + 1;
         }
-        
-        return result;
+        int num_runs = rid;
+        vector<long long> prefix(num_runs + 1, 0LL);
+        for (int k = 1; k <= num_runs; ++k) {
+            prefix[k] = prefix[k - 1] + full_contrib[k - 1];
+        }
+        int qsize = queries.size();
+        vector<long long> ans(qsize);
+        for (int qi = 0; qi < qsize; ++qi) {
+            int l = queries[qi][0];
+            int r = queries[qi][1];
+            int jid = run_id[l];
+            int mid = run_id[r];
+            long long res = 0LL;
+            // left partial
+            int le_start = l;
+            int le_end = std::min(r, run_end_pos[l]);
+            long long le_len = static_cast<long long>(le_end - le_start + 1);
+            res += le_len * (le_len + 1) / 2;
+            if (jid != mid) {
+                // full runs jid+1 to mid-1
+                if (jid + 1 <= mid - 1) {
+                    res += prefix[mid] - prefix[jid + 1];
+                }
+                // right partial
+                int ri_start = run_start_pos[r];
+                int ri_end = r;
+                long long ri_len = static_cast<long long>(ri_end - ri_start + 1);
+                res += ri_len * (ri_len + 1) / 2;
+            }
+            ans[qi] = res;
+        }
+        return ans;
     }
 };
 # @lc code=end
