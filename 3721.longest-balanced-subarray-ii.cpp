a866487e3e7438961d079da0b3e53f1926fffe46
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 19:50:22 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.cpp
+++ 3721.longest-balanced-subarray-ii.cpp
@@ -1,29 +1,25 @@
-#
-# @lc app=leetcode id=3721 lang=cpp
-#
-# [3721] Longest Balanced Subarray II
-#
-# @lc code=start
+//
+// @lc app=leetcode id=3721 lang=cpp
+//
+// [3721] Longest Balanced Subarray II
+//
+// @lc code=start
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
         int maxLen = 0;
         
-        // Try all possible subarrays
         for (int i = 0; i < n; i++) {
-            unordered_set<int> evens;
-            unordered_set<int> odds;
+            unordered_set<int> evens, odds;
             
             for (int j = i; j < n; j++) {
-                // Add current number to appropriate set
                 if (nums[j] % 2 == 0) {
                     evens.insert(nums[j]);
                 } else {
                     odds.insert(nums[j]);
                 }
                 
-                // Check if balanced
                 if (evens.size() == odds.size()) {
                     maxLen = max(maxLen, j - i + 1);
                 }
@@ -33,4 +29,4 @@ public:
         return maxLen;
     }
 };
-# @lc code=end
+// @lc code=end
