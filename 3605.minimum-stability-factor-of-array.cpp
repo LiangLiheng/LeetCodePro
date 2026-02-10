7d049b1d6dc990dba176f8b92d6689285b1b4a8b
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 03:07:19 +0800

    Batch add 1 solutions: 3605.minimum-stabili...

--- 3605.minimum-stability-factor-of-array.cpp
+++ 3605.minimum-stability-factor-of-array.cpp
@@ -3,83 +3,69 @@
 #
 # [3605] Minimum Stability Factor of Array
 #
+
 # @lc code=start
 class Solution {
 public:
-    int gcd(int a, int b) {
-        while (b) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-    
-    bool canAchieve(vector<int>& nums, int maxC, int L) {
-        int n = nums.size();
-        
-        if (L == 0) {
-            // Need to make all elements < 2
-            int count = 0;
-            for (int x : nums) {
-                if (x >= 2) count++;
-            }
-            return count <= maxC;
-        }
-        
-        int modifications = 0;
-        int i = 0;
-        
-        while (i < n) {
-            if (nums[i] < 2) {
-                i++;
-                continue;
-            }
-            
-            // Find the longest stable subarray starting at i
-            int g = nums[i];
-            int j = i + 1;
-            
-            while (j < n) {
-                int new_g = gcd(g, nums[j]);
-                if (new_g < 2) break;
-                g = new_g;
-                j++;
-            }
-            
-            // Stable subarray is [i, j-1] with length j-i
-            int len = j - i;
-            if (len > L) {
-                // Need to place modifications to break it
-                int num_mods = len / (L + 1);
-                modifications += num_mods;
-                if (modifications > maxC) return false;
-            }
-            
-            i = j;
-        }
-        
-        return true;
-    }
-    
     int minStable(vector<int>& nums, int maxC) {
+        auto mygcd = [](int a, int b) -> int {
+            while (b != 0) {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        };
         int n = nums.size();
-        
-        // Binary search on the answer
+        if (n == 0) return 0;
+        std::vector<int> logg(n + 1, 0);
+        for (int i = 2; i <= n; ++i) {
+            logg[i] = logg[i / 2] + 1;
+        }
+        int LOG = logg[n] + 1;
+        std::vector<std::vector<int>> st(LOG, std::vector<int>(n, 0));
+        for (int i = 0; i < n; ++i) {
+            st[0][i] = nums[i];
+        }
+        for (int j = 1; j < LOG; ++j) {
+            for (int i = 0; i + (1 << j) <= n; ++i) {
+                st[j][i] = mygcd(st[j - 1][i], st[j - 1][i + (1 << (j - 1))] );
+            }
+        }
+        auto query = [&](int L, int R) -> int {
+            int len = R - L + 1;
+            int k = logg[len];
+            return mygcd(st[k][L], st[k][R - (1 << k) + 1]);
+        };
+        auto can_achieve = [&](int K) -> bool {
+            int cnt = 0;
+            int last = -1;
+            for (int i = 0; i <= n - K; ++i) {
+                int g = query(i, i + K - 1);
+                if (g >= 2) {
+                    int s = i;
+                    int e = i + K - 1;
+                    if (last < s || last > e) {
+                        last = e;
+                        ++cnt;
+                        if (cnt > maxC) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return cnt <= maxC;
+        };
         int left = 0, right = n;
-        int answer = n;
-        
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (canAchieve(nums, maxC, mid)) {
-                answer = mid;
-                right = mid - 1;
+            if (can_achieve(mid + 1)) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        
-        return answer;
+        return left;
     }
 };
 # @lc code=end
