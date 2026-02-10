a24b53b2b97b7bc80cfe224432a184335a328a96
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 22:17:41 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.cpp
+++ 1367.linked-list-in-binary-tree.cpp
@@ -3,45 +3,42 @@
 #
 # [1367] Linked List in Binary Tree
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if (!root) return false;
-        // Try to match from current node, or from left/right subtree
-        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
-    }
-    
-private:
-    bool dfs(ListNode* head, TreeNode* root) {
-        // If we've matched all of the linked list, success
-        if (!head) return true;
-        // If tree node is null but list isn't finished, no match
-        if (!root) return false;
-        // Values must match, and rest of list must match in left or right subtree
-        if (head->val != root->val) return false;
-        return dfs(head->next, root->left) || dfs(head->next, root->right);
+        auto matches = [&](auto&& self, ListNode* node, TreeNode* tree) -> bool {
+            if (!node) return true;
+            if (!tree || tree->val != node->val) return false;
+            return self(self, node->next, tree->left) || self(self, node->next, tree->right);
+        };
+        auto dfs = [&](auto&& self, TreeNode* node) -> bool {
+            if (!node) return false;
+            return matches(matches, head, node) || self(self, node->left) || self(self, node->right);
+        };
+        return dfs(dfs, root);
     }
 };
 # @lc code=end
