8868f64752e2ba044a5eb19bbd46321bfc9356c9
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 03:33:08 +0800

    Batch add 1 solutions: 3640.trionic-array-i...

--- 3640.trionic-array-ii.cpp
+++ 3640.trionic-array-ii.cpp
@@ -3,67 +3,86 @@
 #
 # [3640] Trionic Array II
 #
+
 # @lc code=start
 class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
         int n = nums.size();
-        const long long NEG_INF = LLONG_MIN / 2;
-        
-        // DP states:
-        // inc: max sum of strictly increasing sequence ending at current position
-        // dec: max sum of inc->dec pattern ending at current position (in dec phase)
-        // tri: max sum of complete trionic pattern ending at current position
-        long long inc = nums[0];
-        long long dec = NEG_INF;
-        long long tri = NEG_INF;
-        long long result = NEG_INF;
-        
-        for (int i = 1; i < n; i++) {
-            long long new_inc, new_dec, new_tri;
-            
-            // Update increasing state
-            if (nums[i] > nums[i-1]) {
-                new_inc = inc + nums[i];
-            } else {
-                new_inc = nums[i];
-            }
-            
-            // Update decreasing state (after peak)
-            if (nums[i] < nums[i-1]) {
-                new_dec = inc + nums[i];  // Start new dec from current inc
-                if (dec != NEG_INF) {
-                    new_dec = max(new_dec, dec + nums[i]);  // Or continue existing dec
-                }
-            } else {
-                new_dec = NEG_INF;
-            }
-            
-            // Update trionic state (after valley, increasing again)
-            if (nums[i] > nums[i-1]) {
-                new_tri = NEG_INF;
-                if (dec != NEG_INF) {
-                    new_tri = max(new_tri, dec + nums[i]);  // Start new inc from dec
-                }
-                if (tri != NEG_INF) {
-                    new_tri = max(new_tri, tri + nums[i]);  // Or continue existing tri
-                }
-            } else {
-                new_tri = NEG_INF;
-            }
-            
-            // Update states
-            inc = new_inc;
-            dec = new_dec;
-            tri = new_tri;
-            
-            // Track maximum trionic sum
-            if (tri != NEG_INF) {
-                result = max(result, tri);
+        const long long NINF = -(1LL << 60);
+        vector<long long> pref(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            pref[i] = pref[i - 1] + (long long)nums[i - 1];
+        }
+        vector<long long> dpl(n);
+        dpl[0] = (long long)nums[0];
+        for (int i = 1; i < n; ++i) {
+            dpl[i] = (long long)nums[i];
+            if (nums[i - 1] < nums[i]) {
+                dpl[i] = max(dpl[i], dpl[i - 1] + (long long)nums[i]);
             }
         }
-        
-        return result;
+        vector<long long> lmax(n, NINF);
+        for (int i = 1; i < n; ++i) {
+            if (nums[i - 1] < nums[i]) {
+                lmax[i] = dpl[i - 1] + (long long)nums[i];
+            }
+        }
+        vector<long long> dpr(n);
+        dpr[n - 1] = (long long)nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            dpr[i] = (long long)nums[i];
+            if (nums[i] < nums[i + 1]) {
+                dpr[i] = max(dpr[i], (long long)nums[i] + dpr[i + 1]);
+            }
+        }
+        vector<long long> rmax(n, NINF);
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i] < nums[i + 1]) {
+                rmax[i] = (long long)nums[i] + dpr[i + 1];
+            }
+        }
+        vector<int> ds(n);
+        ds[0] = 0;
+        for (int i = 1; i < n; ++i) {
+            ds[i] = (nums[i - 1] > nums[i] ? ds[i - 1] : i);
+        }
+        vector<long long> vals(n, NINF);
+        for (int p = 0; p < n; ++p) {
+            if (lmax[p] != NINF) {
+                vals[p] = lmax[p] - pref[p + 1];
+            }
+        }
+        const int LOGN = 18;
+        vector<vector<long long>> st(LOGN, vector<long long>(n, NINF));
+        for (int i = 0; i < n; ++i) {
+            st[0][i] = vals[i];
+        }
+        for (int j = 1; j < LOGN; ++j) {
+            for (int i = 0; i + (1 << j) <= n; ++i) {
+                st[j][i] = max(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+            }
+        }
+        vector<int> logn(n + 1, 0);
+        for (int i = 2; i <= n; ++i) {
+            logn[i] = logn[i / 2] + 1;
+        }
+        auto get_max = [&](int L, int R) -> long long {
+            if (L > R) return NINF;
+            int k = logn[R - L + 1];
+            return max(st[k][L], st[k][R - (1 << k) + 1]);
+        };
+        long long ans = NINF;
+        for (int q = 0; q < n - 1; ++q) {
+            if (rmax[q] == NINF) continue;
+            int L = ds[q];
+            int RR = q - 1;
+            long long bl = get_max(L, RR);
+            if (bl == NINF) continue;
+            long long cur = bl + pref[q + 1] - (long long)nums[q] + rmax[q];
+            ans = max(ans, cur);
+        }
+        return ans;
     }
 };
 # @lc code=end
