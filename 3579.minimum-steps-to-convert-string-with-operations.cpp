3ac08ef4bf9bff08e1febed6080787210b57c0ca
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 19:11:40 +0800

    Batch add 1 solutions: 3579.minimum-steps-t...

--- 3579.minimum-steps-to-convert-string-with-operations.cpp
+++ 3579.minimum-steps-to-convert-string-with-operations.cpp
@@ -13,11 +13,10 @@ public:
         
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] == INT_MAX) continue;
-                string s1 = word1.substr(j, i - j);
-                string s2 = word2.substr(j, i - j);
-                int cost = calcMinOps(s1, s2);
-                dp[i] = min(dp[i], dp[j] + cost);
+                int cost = calculateMinOps(word1, word2, j, i - 1);
+                if (dp[j] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[j] + cost);
+                }
             }
         }
         
@@ -25,42 +24,67 @@ public:
     }
     
 private:
-    int calcMinOps(string s, string t) {
-        int ops1 = calcOpsHelper(s, t, 0);
+    int calculateMinOps(const string& word1, const string& word2, int l, int r) {
+        string s1 = word1.substr(l, r - l + 1);
+        string s2 = word2.substr(l, r - l + 1);
         
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        int ops2 = calcOpsHelper(rev, t, 1);
+        // Try without reversing
+        int cost1 = calculateOpsHelper(s1, s2, false);
         
-        return min(ops1, ops2);
+        // Try with reversing
+        int cost2 = calculateOpsHelper(s1, s2, true);
+        
+        return min(cost1, cost2);
     }
     
-    int calcOpsHelper(string s, string t, int reverseCount) {
-        int n = s.length();
-        vector<bool> used(n, false);
-        int ops = reverseCount;
+    int calculateOpsHelper(string s1, const string& s2, bool useReverse) {
+        int cost = 0;
         
-        // Find positions that can be paired for swaps
-        for (int i = 0; i < n; i++) {
-            if (used[i] || s[i] == t[i]) continue;
-            for (int j = i + 1; j < n; j++) {
-                if (used[j] || s[j] == t[j]) continue;
-                if (s[i] == t[j] && s[j] == t[i]) {
-                    used[i] = used[j] = true;
-                    ops++;
+        if (useReverse) {
+            reverse(s1.begin(), s1.end());
+            cost = 1;
+        }
+        
+        // Find all mismatched positions
+        vector<int> mismatches;
+        for (size_t i = 0; i < s1.length(); i++) {
+            if (s1[i] != s2[i]) {
+                mismatches.push_back(i);
+            }
+        }
+        
+        // Try to pair up mismatches for swaps
+        int swaps = 0;
+        vector<bool> used(s1.length(), false);
+        
+        for (size_t i = 0; i < mismatches.size(); i++) {
+            if (used[mismatches[i]]) continue;
+            
+            for (size_t j = i + 1; j < mismatches.size(); j++) {
+                if (used[mismatches[j]]) continue;
+                
+                int pos_i = mismatches[i];
+                int pos_j = mismatches[j];
+                
+                // Check if swapping these two positions would fix both
+                if (s1[pos_i] == s2[pos_j] && s1[pos_j] == s2[pos_i]) {
+                    swaps++;
+                    used[pos_i] = true;
+                    used[pos_j] = true;
                     break;
                 }
             }
         }
         
         // Count remaining mismatches that need replacements
-        for (int i = 0; i < n; i++) {
-            if (!used[i] && s[i] != t[i]) {
-                ops++;
+        int replacements = 0;
+        for (int pos : mismatches) {
+            if (!used[pos]) {
+                replacements++;
             }
         }
         
-        return ops;
+        return cost + swaps + replacements;
     }
 };
 # @lc code=end
