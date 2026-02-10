fdf405c706075fd9ada45d2da40c78a9679fa73e
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 20:03:02 +0800

    Batch add 1 solutions: 3748.count-stable-su...

--- 3748.count-stable-subarrays.cpp
+++ 3748.count-stable-subarrays.cpp
@@ -7,32 +7,24 @@
 class Solution {
 public:
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> max_reach(n);
-        
-        // Precompute max_reach for each position
-        // max_reach[i] = maximum index j such that nums[i..j] is non-decreasing
-        for (int i = n - 1; i >= 0; i--) {
-            if (i == n - 1) {
-                max_reach[i] = i;
-            } else if (nums[i] <= nums[i + 1]) {
-                max_reach[i] = max_reach[i + 1];
-            } else {
-                max_reach[i] = i;
-            }
-        }
-        
-        // Process queries
         vector<long long> result;
+        
         for (auto& query : queries) {
-            int l = query[0], r = query[1];
+            int l = query[0];
+            int r = query[1];
             long long count = 0;
             
-            // For each starting position in [l, r]
-            for (int i = l; i <= r; i++) {
-                // Count stable subarrays starting at i
-                // They can extend up to min(max_reach[i], r)
-                count += min(max_reach[i], r) - i + 1;
+            int i = l;
+            while (i <= r) {
+                int j = i;
+                // Extend j to find maximal non-decreasing sequence
+                while (j < r && nums[j] <= nums[j + 1]) {
+                    j++;
+                }
+                // Count subarrays in this non-decreasing sequence
+                long long len = j - i + 1;
+                count += len * (len + 1) / 2;
+                i = j + 1;
             }
             
             result.push_back(count);
