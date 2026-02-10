64728fbac2809c570514acf11d598c2ed2a293c2
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 15:50:31 +0800

    Batch add 1 solutions: 3725.count-ways-to-c...

--- 3725.count-ways-to-choose-coprime-integers-from-rows.cpp
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.cpp
@@ -8,60 +8,31 @@ class Solution {
 public:
     int countCoprime(vector<vector<int>>& mat) {
         const int MOD = 1e9 + 7;
-        const int MAX_VAL = 150;
         int m = mat.size();
         
-        // Compute Möbius function using sieve
-        vector<int> mu(MAX_VAL + 1);
-        vector<int> spf(MAX_VAL + 1); // smallest prime factor
+        // dp[g] = number of ways to achieve GCD = g
+        unordered_map<int, long long> dp;
         
-        for (int i = 1; i <= MAX_VAL; i++) {
-            spf[i] = i;
+        // Initialize with first row
+        for (int val : mat[0]) {
+            dp[val]++;
         }
         
-        for (int i = 2; i <= MAX_VAL; i++) {
-            if (spf[i] == i) { // i is prime
-                for (int j = i; j <= MAX_VAL; j += i) {
-                    if (spf[j] == j) {
-                        spf[j] = i;
-                    }
+        // Process remaining rows
+        for (int i = 1; i < m; i++) {
+            unordered_map<int, long long> ndp;
+            
+            for (auto& [g, cnt] : dp) {
+                for (int val : mat[i]) {
+                    int new_g = __gcd(g, val);
+                    ndp[new_g] = (ndp[new_g] + cnt) % MOD;
                 }
             }
+            
+            dp = move(ndp);
         }
         
-        mu[1] = 1;
-        for (int i = 2; i <= MAX_VAL; i++) {
-            int p = spf[i];
-            int rest = i / p;
-            
-            if (rest % p == 0) {
-                mu[i] = 0;
-            } else {
-                mu[i] = -mu[rest];
-            }
-        }
-        
-        // Count ways for each divisor
-        long long result = 0;
-        
-        for (int d = 1; d <= MAX_VAL; d++) {
-            if (mu[d] == 0) continue;
-            
-            long long ways = 1;
-            for (int i = 0; i < m; i++) {
-                int count = 0;
-                for (int num : mat[i]) {
-                    if (num % d == 0) {
-                        count++;
-                    }
-                }
-                ways = (ways * count) % MOD;
-            }
-            
-            result = (result + (long long)mu[d] * ways) % MOD;
-        }
-        
-        return (result % MOD + MOD) % MOD;
+        return dp[1];
     }
 };
 # @lc code=end
