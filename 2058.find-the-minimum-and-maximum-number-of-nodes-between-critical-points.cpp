1bf6d7025f85483509ba28ec27e8d0f3cd609481
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 22:22:39 +0800

    Batch add 1 solutions: 2058.find-the-minimu...

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -3,59 +3,45 @@
 #
 # [2058] Find the Minimum and Maximum Number of Nodes Between Critical Points
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> criticalPoints;
-        
-        // We need at least 3 nodes to have a critical point
-        if (!head || !head->next || !head->next->next) {
-            return {-1, -1};
-        }
-        
-        ListNode* prev = head;
-        ListNode* curr = head->next;
-        int position = 1; // Position starts from 1 (second node)
-        
-        while (curr->next) {
-            // Check if current node is a critical point
-            bool isMaxima = curr->val > prev->val && curr->val > curr->next->val;
-            bool isMinima = curr->val < prev->val && curr->val < curr->next->val;
-            
-            if (isMaxima || isMinima) {
-                criticalPoints.push_back(position);
+        vector<int> crit;
+        if (!head || !head->next) return {-1, -1};
+        ListNode* p = head;
+        ListNode* c = head->next;
+        int pos = 2;
+        while (c->next) {
+            ListNode* n = c->next;
+            if ((c->val > p->val && c->val > n->val) || 
+                (c->val < p->val && c->val < n->val)) {
+                crit.push_back(pos);
             }
-            
-            prev = curr;
-            curr = curr->next;
-            position++;
+            p = c;
+            c = n;
+            ++pos;
         }
-        
-        // If less than 2 critical points, return [-1, -1]
-        if (criticalPoints.size() < 2) {
+        if (crit.size() < 2) {
             return {-1, -1};
         }
-        
-        // Calculate min and max distances
-        int minDistance = INT_MAX;
-        int maxDistance = criticalPoints.back() - criticalPoints[0];
-        
-        for (int i = 1; i < criticalPoints.size(); i++) {
-            minDistance = min(minDistance, criticalPoints[i] - criticalPoints[i-1]);
+        int min_dist = crit[1] - crit[0];
+        int max_dist = crit.back() - crit[0];
+        for (size_t i = 2; i < crit.size(); ++i) {
+            min_dist = min(min_dist, crit[i] - crit[i - 1]);
         }
-        
-        return {minDistance, maxDistance};
+        return {min_dist, max_dist};
     }
 };
 # @lc code=end
