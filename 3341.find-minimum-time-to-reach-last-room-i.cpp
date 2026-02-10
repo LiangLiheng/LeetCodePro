2b40952966bf98f2b43d163174d2b2992e0d2675
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-10 23:12:27 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.cpp
+++ 3341.find-minimum-time-to-reach-last-room-i.cpp
@@ -8,31 +8,29 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size();
-        int m = moveTime[0].size();
-        vector<vector<long long>> dist(n, vector<long long>(m, LLONG_MAX / 2));
+        int rows = moveTime.size();
+        int cols = moveTime[0].size();
+        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
         dist[0][0] = 0;
-        using ll = long long;
-        priority_queue<tuple<ll, int, int>, vector<tuple<ll, int, int>>, greater<tuple<ll, int, int>>> pq;
-        pq.push({0LL, 0, 0});
-        int di[4] = {-1, 0, 1, 0};
-        int dj[4] = {0, 1, 0, -1};
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        pq.push({0, 0, 0});
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while (!pq.empty()) {
-            auto [time, i, j] = pq.top(); pq.pop();
-            if (time > dist[i][j]) continue;
-            for (int d = 0; d < 4; ++d) {
-                int ni = i + di[d];
-                int nj = j + dj[d];
-                if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
-                    ll ntime = max(time, (ll)moveTime[ni][nj]) + 1;
-                    if (ntime < dist[ni][nj]) {
-                        dist[ni][nj] = ntime;
-                        pq.push({ntime, ni, nj});
+            auto [time, r, c] = pq.top(); pq.pop();
+            if (time > dist[r][c]) continue;
+            for (auto& dir : dirs) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                    int new_time = max(time, moveTime[nr][nc]) + 1;
+                    if (new_time < dist[nr][nc]) {
+                        dist[nr][nc] = new_time;
+                        pq.push({new_time, nr, nc});
                     }
                 }
             }
         }
-        return dist[n - 1][m - 1];
+        return dist[rows - 1][cols - 1];
     }
 };
 # @lc code=end
