1779b0f47f34f9ae185dffd63f077844bc9a6a91
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 00:30:33 +0800

    Batch add 1 solutions: 3457.eat-pizzas.cpp...

--- 3457.eat-pizzas.cpp
+++ 3457.eat-pizzas.cpp
@@ -3,33 +3,24 @@
 #
 # [3457] Eat Pizzas!
 #
+
 # @lc code=start
 class Solution {
 public:
     long long maxWeight(vector<int>& pizzas) {
-        sort(pizzas.begin(), pizzas.end(), greater<int>());
-        
+        std::sort(pizzas.rbegin(), pizzas.rend());
         int n = pizzas.size();
-        int days = n / 4;
-        long long totalWeight = 0;
-        int left = 0, right = n - 1;
-        
-        for (int day = 1; day <= days; day++) {
-            if (day % 2 == 1) {  // Odd day
-                // Take 1 from left (largest) and 3 from right (smallest)
-                totalWeight += pizzas[left];
-                left++;
-                right -= 3;
-            } else {  // Even day
-                // Take 2 from left and 2 from right
-                // Gain the second largest, which is pizzas[left + 1]
-                totalWeight += pizzas[left + 1];
-                left += 2;
-                right -= 2;
-            }
+        int k = n / 4;
+        int even_days = k / 2;
+        int odd_days = k - even_days;
+        long long total = 0;
+        for (int i = 0; i < odd_days; ++i) {
+            total += pizzas[i];
         }
-        
-        return totalWeight;
+        for (int i = 0; i < even_days; ++i) {
+            total += pizzas[odd_days + 2 * i + 1];
+        }
+        return total;
     }
 };
 # @lc code=end
