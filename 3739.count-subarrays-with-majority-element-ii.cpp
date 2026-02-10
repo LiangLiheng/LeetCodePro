9dc7276fb7bb375e450929e7530fe16388896b08
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 04:15:17 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.cpp
+++ 3739.count-subarrays-with-majority-element-ii.cpp
@@ -3,31 +3,49 @@
 #
 # [3739] Count Subarrays With Majority Element II
 #
+
 # @lc code=start
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        long long count = 0;
-        
-        map<long long, int> prefixCount;
-        prefixCount[0] = 1;
-        
-        long long prefixSum = 0;
-        
-        for (int i = 0; i < n; i++) {
-            prefixSum += (nums[i] == target) ? 1 : -1;
-            
-            // Count all prefixes with sum < prefixSum
-            auto it = prefixCount.lower_bound(prefixSum);
-            for (auto iter = prefixCount.begin(); iter != it; ++iter) {
-                count += iter->second;
-            }
-            
-            prefixCount[prefixSum]++;
+        vector<int> a(n);
+        for(int i = 0; i < n; ++i) {
+            a[i] = (nums[i] == target ? 1 : -1);
         }
-        
-        return count;
+        vector<long long> prefix(n + 1, 0LL);
+        for(int i = 1; i <= n; ++i) {
+            prefix[i] = prefix[i - 1] + a[i - 1];
+        }
+        auto vals = prefix;
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+        int m = vals.size();
+        vector<int> rank(n + 1);
+        for(int i = 0; i <= n; ++i) {
+            rank[i] = lower_bound(vals.begin(), vals.end(), prefix[i]) - vals.begin() + 1;
+        }
+        struct Fenwick {
+            int n;
+            vector<long long> t;
+            Fenwick(int _n) : n(_n), t(_n + 1, 0LL) {};
+            void update(int i, long long v) {
+                for (; i <= n; i += i & -i) t[i] += v;
+            }
+            long long query(int i) {
+                long long s = 0;
+                for (; i > 0; i -= i & -i) s += t[i];
+                return s;
+            }
+        };
+        Fenwick ft(m);
+        long long ans = 0;
+        ft.update(rank[0], 1LL);
+        for(int r = 1; r <= n; ++r) {
+            ans += ft.query(rank[r] - 1);
+            ft.update(rank[r], 1LL);
+        }
+        return ans;
     }
 };
 # @lc code=end
