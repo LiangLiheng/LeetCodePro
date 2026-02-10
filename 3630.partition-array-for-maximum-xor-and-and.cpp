2914490045f5c986aba17430af8c1809574c37ff
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 03:33:45 +0800

    Batch add 1 solutions: 3630.partition-array...

--- 3630.partition-array-for-maximum-xor-and-and.cpp
+++ 3630.partition-array-for-maximum-xor-and-and.cpp
@@ -3,50 +3,42 @@
 #
 # [3630] Partition Array for Maximum XOR and AND
 #
+
 # @lc code=start
 class Solution {
 public:
     long long maximizeXorAndXor(vector<int>& nums) {
         int n = nums.size();
-        long long maxSum = 0;
-        
-        // Calculate total number of partitions (3^n)
-        int totalPartitions = 1;
-        for (int i = 0; i < n; i++) {
-            totalPartitions *= 3;
+        int N = 1 << n;
+        vector<long long> xor_all(N, 0);
+        vector<long long> and_all(N, 0);
+        and_all[0] = (1LL << 40) - 1;
+        for (int mask = 1; mask < N; ++mask) {
+            int i = __builtin_ctz(mask);
+            int prev = mask ^ (1 << i);
+            xor_all[mask] = xor_all[prev] ^ (long long)nums[i];
+            and_all[mask] = and_all[prev] & (long long)nums[i];
         }
-        
-        // Try all possible partitions using ternary representation
-        for (int mask = 0; mask < totalPartitions; mask++) {
-            int temp = mask;
-            long long xorA = 0, xorC = 0;
-            long long andB = 0;
-            bool hasB = false;
-            
-            // Decode ternary mask: 0=A, 1=B, 2=C
-            for (int i = 0; i < n; i++) {
-                int group = temp % 3;
-                temp /= 3;
-                
-                if (group == 0) {
-                    xorA ^= nums[i];
-                } else if (group == 1) {
-                    if (!hasB) {
-                        andB = nums[i];
-                        hasB = true;
-                    } else {
-                        andB &= nums[i];
-                    }
-                } else {
-                    xorC ^= nums[i];
-                }
+        vector<long long> maxxs(N, 0);
+        for (int masks = 0; masks < N; ++masks) {
+            long long sx = xor_all[masks];
+            long long cur = sx;
+            for (int sub = masks; sub > 0; sub = (sub - 1) & masks) {
+                long long y = xor_all[sub];
+                long long val = y + (sx ^ y);
+                if (val > cur) cur = val;
             }
-            
-            long long sum = xorA + andB + xorC;
-            maxSum = max(maxSum, sum);
+            maxxs[masks] = cur;
         }
-        
-        return maxSum;
+        long long ans = 0;
+        int full = N - 1;
+        for (int maskb = 0; maskb < N; ++maskb) {
+            long long andb = (maskb == 0 ? 0LL : and_all[maskb]);
+            int masks = full ^ maskb;
+            long long val = andb + maxxs[masks];
+            if (val > ans) ans = val;
+        }
+        return ans;
     }
 };
 # @lc code=end
