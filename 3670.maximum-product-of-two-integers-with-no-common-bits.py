c5a1340a0d5ec45140e18e4472f0845d1bb53f2d
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:03:46 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.py
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.py
@@ -7,49 +7,25 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        class TrieNode:
-            def __init__(self):
-                self.children = [None, None]
-                self.max_val = 0
-
-        def insert(root, num):
-            node = root
-            node.max_val = max(node.max_val, num)
-            for i in range(19, -1, -1):
-                bit = (num >> i) & 1
-                if node.children[bit] is None:
-                    node.children[bit] = TrieNode()
-                node = node.children[bit]
-                node.max_val = max(node.max_val, num)
-
-        def getMax(root, x):
-            if root.max_val == 0:
-                return 0
-            node = root
-            res = 0
-            for i in range(19, -1, -1):
-                bit = (x >> i) & 1
-                if bit == 1:
-                    if node.children[0] is None:
-                        return 0
-                    node = node.children[0]
-                else:
-                    if node.children[1] is not None:
-                        res |= (1 << i)
-                        node = node.children[1]
-                    elif node.children[0] is not None:
-                        node = node.children[0]
-                    else:
-                        return 0
-            return res
-
-        root = TrieNode()
-        ans = 0
+        from collections import defaultdict
+        bitmask_to_values = defaultdict(list)
         for num in nums:
-            cand = getMax(root, num)
-            if cand > 0:
-                ans = max(ans, cand * num)
-            insert(root, num)
-        return ans
-
+            bm = num
+            bitmask_to_values[bm].append(num)
+        # For each bitmask, keep at most the two largest values
+        for bm in bitmask_to_values:
+            bitmask_to_values[bm].sort(reverse=True)
+            if len(bitmask_to_values[bm]) > 2:
+                bitmask_to_values[bm] = bitmask_to_values[bm][:2]
+        unique_bitmasks = list(bitmask_to_values.keys())
+        max_product = 0
+        for i in range(len(unique_bitmasks)):
+            bm1 = unique_bitmasks[i]
+            v1s = bitmask_to_values[bm1]
+            for j in range(i+1, len(unique_bitmasks)):
+                bm2 = unique_bitmasks[j]
+                if bm1 & bm2 == 0:
+                    v2s = bitmask_to_values[bm2]
+                    max_product = max(max_product, v1s[0] * v2s[0])
+        return max_product
 # @lc code=end
