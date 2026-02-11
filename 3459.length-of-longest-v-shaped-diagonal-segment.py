3090a2035ae68e7ccab24585f7d00e6150b8deea
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:56:28 +0800

    Batch add 1 solutions: 3459.length-of-longe...

--- 3459.length-of-longest-v-shaped-diagonal-segment.py
+++ 3459.length-of-longest-v-shaped-diagonal-segment.py
@@ -5,45 +5,42 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)] # four diagonals
+        # clockwise order: 0->1->3->2
+        cw = {0:1, 1:3, 3:2, 2:0}
         from functools import lru_cache
-        n = len(grid)
-        if n == 0:
-            return 0
-        m = len(grid[0])
-        dirs = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+
+        seq = [2,0] # after 1, alternates
+
         @lru_cache(None)
-        def dfs(r, c, d, turned, par):
+        def dfs(i, j, d, turn, k):
+            # i,j: current pos
+            # d: direction
+            # turn: 0 (no turn yet), 1 (already turned)
+            # k: index in seq: 0 for 2, 1 for 0
             res = 0
-            # continue straight
-            dr, dc = dirs[d]
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < n and 0 <= nc < m:
-                npar = 1 - par
-                exp = 2 if npar == 1 else 0
-                if grid[nr][nc] == exp:
-                    res = max(res, 1 + dfs(nr, nc, d, turned, npar))
-            # turn clockwise if not turned
-            if turned == 0:
-                nd = (d + 1) % 4
-                dr, dc = dirs[nd]
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0 <= nc < m:
-                    npar = 1 - par
-                    exp = 2 if npar == 1 else 0
-                    if grid[nr][nc] == exp:
-                        res = max(res, 1 + dfs(nr, nc, nd, 1, npar))
+            ni, nj = i+dirs[d][0], j+dirs[d][1]
+            # Explicitly check boundaries and handle edge cases
+            if 0 <= ni < n and 0 <= nj < m and grid[ni][nj] == seq[k]:
+                res = max(res, 1 + dfs(ni, nj, d, turn, k^1))
+            # Try turning if not turned yet
+            if turn==0:
+                d2 = cw[d]
+                ni2, nj2 = i+dirs[d2][0], j+dirs[d2][1]
+                if 0 <= ni2 < n and 0 <= nj2 < m and grid[ni2][nj2] == seq[k]:
+                    res = max(res, 1 + dfs(ni2, nj2, d2, 1, k^1))
             return res
+
         ans = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == 1:
-                    ans = max(ans, 1)
+                if grid[i][j]==1:
                     for d in range(4):
-                        dr, dc = dirs[d]
-                        ni, nj = i + dr, j + dc
-                        if 0 <= ni < n and 0 <= nj < m and grid[ni][nj] == 2:
-                            ans = max(ans, 2 + dfs(ni, nj, d, 0, 1))
+                        # After each segment, verify all constraints are met by construction
+                        ans = max(ans, 1 + dfs(i, j, d, 0, 0))
         return ans
 # @lc code=end
