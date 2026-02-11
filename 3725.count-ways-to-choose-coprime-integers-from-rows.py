ebcd4e73663434671abfe659429f8f3464cefd9e
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:09:40 +0800

    Batch add 1 solutions: 3725.count-ways-to-c...

--- 3725.count-ways-to-choose-coprime-integers-from-rows.py
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.py
@@ -5,52 +5,23 @@
 #
 
 # @lc code=start
+from typing import List
+from math import gcd
+from collections import defaultdict
+
 class Solution:
     def countCoprime(self, mat: List[List[int]]) -> int:
         MOD = 10**9 + 7
-        MAXN = 150
-        # Compute Mobius function
-        mu = [0] * (MAXN + 1)
-        is_composite = [False] * (MAXN + 1)
-        primes = []
-        mu[1] = 1
-        for i in range(2, MAXN + 1):
-            if not is_composite[i]:
-                primes.append(i)
-                mu[i] = -1
-            for p in primes:
-                if i * p > MAXN:
-                    break
-                is_composite[i * p] = True
-                if i % p == 0:
-                    mu[i * p] = 0
-                    break
-                else:
-                    mu[i * p] = -mu[i]
-        # Frequency per row
         m = len(mat)
-        freq = [[0] * (MAXN + 1) for _ in range(m)]
-        for i in range(m):
-            for x in mat[i]:
-                if 1 <= x <= MAXN:
-                    freq[i][x] += 1
-        # Compute answer
-        ans = 0
-        for d in range(1, MAXN + 1):
-            if mu[d] == 0:
-                continue
-            ways = 1
-            zero_count = False
-            for i in range(m):
-                cnt = sum(freq[i][j] for j in range(d, MAXN + 1, d))
-                if cnt == 0:
-                    zero_count = True
-                    break
-                ways = (ways * cnt) % MOD
-            if zero_count:
-                continue
-            contrib = (mu[d] * ways) % MOD
-            ans = (ans + contrib) % MOD
-        return ans
-
+        dp = defaultdict(int)
+        for num in mat[0]:
+            dp[num] += 1
+        for i in range(1, m):
+            new_dp = defaultdict(int)
+            for prev_gcd, count in dp.items():
+                for num in mat[i]:
+                    ngcd = gcd(prev_gcd, num)
+                    new_dp[ngcd] = (new_dp[ngcd] + count) % MOD
+            dp = new_dp
+        return dp[1] if 1 in dp else 0
 # @lc code=end
