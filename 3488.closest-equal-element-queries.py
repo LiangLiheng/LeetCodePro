c3e31f4ad00b2e7b168a62b54f9abc0f5cb6f4ec
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 04:03:33 +0800

    Batch add 1 solutions: 3488.closest-equal-e...

--- 3488.closest-equal-element-queries.py
+++ 3488.closest-equal-element-queries.py
@@ -6,27 +6,28 @@
 
 # @lc code=start
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         n = len(nums)
-        positions = {}
-        for i, num in enumerate(nums):
-            if num not in positions:
-                positions[num] = []
-            positions[num].append(i)
-        min_dist = [-1] * n
-        for pos in positions.values():
-            k = len(pos)
-            if k > 1:
-                for i in range(k):
-                    left_i = (i - 1 + k) % k
-                    d_left = abs(pos[i] - pos[left_i])
-                    d_left = min(d_left, n - d_left)
-                    right_i = (i + 1) % k
-                    d_right = abs(pos[i] - pos[right_i])
-                    d_right = min(d_right, n - d_right)
-                    min_dist[pos[i]] = min(d_left, d_right)
-        ans = [min_dist[q] for q in queries]
-        return ans
+        val_to_indices = defaultdict(list)
+        for idx, val in enumerate(nums):
+            val_to_indices[val].append(idx)
+        res = []
+        for q in queries:
+            val = nums[q]
+            idxs = val_to_indices[val]
+            if len(idxs) == 1:
+                res.append(-1)
+                continue
+            min_dist = n
+            for idx in idxs:
+                if idx == q:
+                    continue
+                dist = abs(q - idx)
+                circ_dist = min(dist, n - dist)
+                min_dist = min(min_dist, circ_dist)
+            res.append(min_dist)
+        return res
 # @lc code=end
