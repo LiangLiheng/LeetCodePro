503561927d18dd21abccee2e534ea2dd5ab6dd63
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 12:44:03 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.py
+++ 3217.delete-nodes-from-linked-list-present-in-array.py
@@ -3,26 +3,22 @@
 #
 # [3217] Delete Nodes From Linked List Present in Array
 #
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-from typing import List, Optional
-
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         num_set = set(nums)
-        dummy = ListNode(0, head)
-        cur = dummy
-        while cur.next:
-            if cur.next.val in num_set:
-                cur.next = cur.next.next
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        while prev.next:
+            if prev.next.val in num_set:
+                prev.next = prev.next.next
             else:
-                cur = cur.next
+                prev = prev.next
         return dummy.next
-
 # @lc code=end
