5d7f56e14b4fca004e27a87de9acd38a7e408012
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 12:39:34 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -5,19 +5,19 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        ans = 0
         stack = []
-        for i in range(len(nums)):
-            cur_max_steps = 0
-            while stack and nums[stack[-1][0]] <= nums[i]:
-                _, mstep = stack.pop()
-                cur_max_steps = max(cur_max_steps, mstep)
+        ans = 0
+        for num in reversed(nums):
             steps = 0
-            if stack:
-                steps = 1 + cur_max_steps
-                ans = max(ans, steps)
-            stack.append((i, max(steps, cur_max_steps)))
+            while stack and stack[-1] < num:
+                steps += 1
+                stack.pop()
+            ans = max(ans, steps)
+            stack.append(num)
         return ans
+
 # @lc code=end
