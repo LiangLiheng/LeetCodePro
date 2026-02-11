450cef2f7534774a50188a46e525cd4dd3b52106
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 13:51:15 +0800

    Batch add 1 solutions: 3425.longest-special...

--- 3425.longest-special-path.py
+++ 3425.longest-special-path.py
@@ -5,61 +5,51 @@
 #
 
 # @lc code=start
-from typing import List
-from collections import deque, defaultdict
-import sys
-
-sys.setrecursionlimit(10**5 + 10)
-
 class Solution:
     def longestSpecialPath(self, edges: List[List[int]], nums: List[int]) -> List[int]:
         n = len(nums)
         adj = [[] for _ in range(n)]
-        for a, b, w in edges:
-            adj[a].append((b, w))
-            adj[b].append((a, w))
-        children = [[] for _ in range(n)]
-        visited = [False] * n
-        q = deque([0])
-        visited[0] = True
-        while q:
-            u = q.popleft()
-            for v, w in adj[u]:
-                if not visited[v]:
-                    visited[v] = True
-                    children[u].append((v, w))
-                    q.append(v)
-        self.ans = [0, 1]
-        path_prefix = []
+        for a, b, c in edges:
+            adj[a].append((b, c))
+            adj[b].append((a, c))
+        self.max_len = 0
+        self.min_nodes = n + 1
+        path_values = []
+        path_dist = []
         last_pos = {}
-        self._dfs(0, 0, path_prefix, 0, last_pos, nums, children)
-        return self.ans
-    
-    def _dfs(self, u: int, w_from_par: int, path_prefix: List[int], curr_start: int, last_pos: dict, nums: List[int], children: List[List]):
-        index = len(path_prefix)
-        old_pos = last_pos.get(nums[u], -1)
-        new_start = curr_start
-        if old_pos >= new_start:
-            new_start = old_pos + 1
-        last_pos[nums[u]] = index
-        new_prefix = (path_prefix[-1] + w_from_par if path_prefix else 0)
-        path_prefix.append(new_prefix)
-        # update ans
-        path_len = new_prefix - path_prefix[new_start]
-        path_nodes = index - new_start + 1
-        if path_len > self.ans[0]:
-            self.ans[0] = path_len
-            self.ans[1] = path_nodes
-        elif path_len == self.ans[0]:
-            self.ans[1] = min(self.ans[1], path_nodes)
-        # recurse
-        for v, w in children[u]:
-            self._dfs(v, w, path_prefix, new_start, last_pos, nums, children)
-        # backtrack
-        path_prefix.pop()
-        if old_pos == -1:
-            del last_pos[nums[u]]
-        else:
-            last_pos[nums[u]] = old_pos
+        window_left = [0]
+        def dfs(u, p, dist):
+            old_left = window_left[0]
+            old_pos_val = last_pos.get(nums[u], -1)
+            conflict = last_pos.get(nums[u], -1)
+            new_left = window_left[0]
+            if conflict >= window_left[0]:
+                new_left = conflict + 1
+            window_left[0] = new_left
+            last_pos[nums[u]] = len(path_values)
+            path_values.append(nums[u])
+            path_dist.append(dist)
+            curr_depth = len(path_values) - 1
+            curr_len = path_dist[curr_depth] - path_dist[window_left[0]]
+            curr_nodes = curr_depth - window_left[0] + 1
+            if curr_len > self.max_len:
+                self.max_len = curr_len
+                self.min_nodes = curr_nodes
+            elif curr_len == self.max_len:
+                self.min_nodes = min(self.min_nodes, curr_nodes)
+            for v, w in adj[u]:
+                if v != p:
+                    dfs(v, u, dist + w)
+            path_values.pop()
+            path_dist.pop()
+            window_left[0] = old_left
+            if old_pos_val == -1:
+                last_pos.pop(nums[u])
+            else:
+                last_pos[nums[u]] = old_pos_val
+        import sys
+        sys.setrecursionlimit(10**5 + 10)
+        dfs(0, -1, 0)
+        return [self.max_len, self.min_nodes]
 
 # @lc code=end
