e511fe6e6cb32ee2a06a91915e95e34cbcb093d7
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:22:11 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.py
+++ 3341.find-minimum-time-to-reach-last-room-i.py
@@ -5,26 +5,28 @@
 #
 
 # @lc code=start
+from typing import List
+import heapq
+
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        import heapq
         n, m = len(moveTime), len(moveTime[0])
-        INF = 10**18 + 5
-        dist = [[INF] * m for _ in range(n)]
+        dist = [[float('inf')] * m for _ in range(n)]
         dist[0][0] = 0
-        pq = [(0, 0, 0)]  # time, row, col
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        while pq:
-            time, i, j = heapq.heappop(pq)
-            if time > dist[i][j]:
+        heap = [(0, 0, 0)]  # (current_time, row, col)
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        while heap:
+            t, r, c = heapq.heappop(heap)
+            if (r, c) == (n-1, m-1):
+                return t
+            if t > dist[r][c]:
                 continue
-            for di, dj in dirs:
-                ni, nj = i + di, j + dj
-                if 0 <= ni < n and 0 <= nj < m:
-                    new_time = max(time, moveTime[ni][nj]) + 1
-                    if new_time < dist[ni][nj]:
-                        dist[ni][nj] = new_time
-                        heapq.heappush(pq, (new_time, ni, nj))
-        return dist[n-1][m-1]
-
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    next_time = max(t + 1, moveTime[nr][nc])
+                    if next_time < dist[nr][nc]:
+                        dist[nr][nc] = next_time
+                        heapq.heappush(heap, (next_time, nr, nc))
+        return -1
 # @lc code=end
