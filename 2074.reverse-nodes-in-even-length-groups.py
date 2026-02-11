08306adec903d8d31a1946ceaefed5a42f764aeb
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 12:34:13 +0800

    Batch add 1 solutions: 2074.reverse-nodes-i...

--- 2074.reverse-nodes-in-even-length-groups.py
+++ 2074.reverse-nodes-in-even-length-groups.py
@@ -13,33 +13,30 @@
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        prev_group_end = dummy
+        prev_tail = dummy
         group_size = 1
-        while prev_group_end.next:
-            group_start = prev_group_end.next
-            group_end = group_start
-            count = 1
-            while count < group_size and group_end.next:
-                group_end = group_end.next
-                count += 1
-            actual_len = count
-            next_start = group_end.next
-            if actual_len % 2 == 0:
-                # Iteratively reverse the first actual_len nodes
-                prevv = None
-                curr = group_start
-                for _ in range(actual_len):
-                    nxt = curr.next
-                    curr.next = prevv
-                    prevv = curr
-                    curr = nxt
-                # prevv is new head, group_start is new tail
-                prev_group_end.next = prevv
-                group_start.next = next_start
-                prev_group_end = group_start
+        while prev_tail.next:
+            group_start = prev_tail.next
+            cur = group_start
+            group_len = 0
+            group_end = None
+            while cur and group_len < group_size:
+                group_len += 1
+                group_end = cur
+                cur = cur.next
+            if group_len % 2 == 0:
+                reversed_head = None
+                rev_cur = group_start
+                for _ in range(group_len):
+                    nxt = rev_cur.next
+                    rev_cur.next = reversed_head
+                    reversed_head = rev_cur
+                    rev_cur = nxt
+                prev_tail.next = reversed_head
+                group_start.next = cur
+                prev_tail = group_start
             else:
-                prev_group_end = group_end
+                prev_tail = group_end
             group_size += 1
         return dummy.next
-
 # @lc code=end
