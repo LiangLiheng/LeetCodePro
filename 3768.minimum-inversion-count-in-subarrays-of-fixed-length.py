0db5c74487f7f3191d4f92a99ea15251f30452d7
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:25:58 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
@@ -5,45 +5,54 @@
 #
 
 # @lc code=start
+from typing import List
+
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size + 2
+        self.tree = [0] * self.n
+    def update(self, i, delta):
+        i += 1
+        while i < self.n:
+            self.tree[i] += delta
+            i += i & -i
+    def query(self, i):
+        res = 0
+        i += 1
+        while i > 0:
+            res += self.tree[i]
+            i -= i & -i
+        return res
+    def query_range(self, l, r):
+        return self.query(r) - self.query(l - 1)
+
 class Solution:
     def minInversionCount(self, nums: List[int], k: int) -> int:
-        class FenwickTree:
-            def __init__(self, size: int):
-                self.size = size
-                self.tree = [0] * (size + 2)
-            def update(self, index: int, delta: int) -> None:
-                while index <= self.size:
-                    self.tree[index] += delta
-                    index += index & -index
-            def query(self, index: int) -> int:
-                res = 0
-                while index > 0:
-                    res += self.tree[index]
-                    index -= index & -index
-                return res
         n = len(nums)
-        vals = sorted(set(nums))
-        rank = {v: i + 1 for i, v in enumerate(vals)}
-        m = len(vals)
-        ft = FenwickTree(m)
-        inv = 0
-        for i in range(k):
-            rnk = rank[nums[i]]
-            inv += ft.query(m) - ft.query(rnk)
-            ft.update(rnk, 1)
-        ans = inv
-        for i in range(1, n - k + 1):
-            rnk_out = rank[nums[i - 1]]
-            delta_out = ft.query(rnk_out - 1)
-            inv -= delta_out
-            ft.update(rnk_out, -1)
-            idx_in = i + k - 1
-            rnk_in = rank[nums[idx_in]]
-            cnt_leq = ft.query(rnk_in)
-            delta_in = (k - 1) - cnt_leq
-            inv += delta_in
-            ft.update(rnk_in, 1)
-            ans = min(ans, inv)
-        return ans
+        # Edge case: window of size 1
+        if k == 1:
+            return 0
+        # Coordinate compression
+        sorted_unique = sorted(set(nums))
+        mp = {v: i for i, v in enumerate(sorted_unique)}
+        arr = [mp[x] for x in nums]
+        max_val = len(sorted_unique)
 
+        bit = FenwickTree(max_val)
+        inversion = 0
+        # First window
+        for i in range(k):
+            inversion += bit.query_range(arr[i] + 1, max_val - 1)
+            bit.update(arr[i], 1)
+        min_inv = inversion
+
+        for i in range(k, n):
+            left = arr[i - k]
+            bit.update(left, -1)
+            inversion -= bit.query_range(0, left - 1)
+            right = arr[i]
+            inversion += bit.query_range(right + 1, max_val - 1)
+            bit.update(right, 1)
+            min_inv = min(min_inv, inversion)
+        return min_inv
 # @lc code=end
