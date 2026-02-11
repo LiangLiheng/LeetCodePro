09a5dbe38da66fca537ffebbaec392168b84e0cd
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 12:56:17 +0800

    Batch add 1 solutions: 3367.maximize-sum-of...

--- 3367.maximize-sum-of-weights-after-edge-removals.py
+++ 3367.maximize-sum-of-weights-after-edge-removals.py
@@ -5,17 +5,41 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maximizeSumOfWeights(self, edges: List[List[int]], k: int) -> int:
+        import sys
+        sys.setrecursionlimit(100010)
         n = len(edges) + 1
-        selected_deg = [0] * n
-        edges.sort(key=lambda e: -e[2])
-        ans = 0
-        for edge in edges:
-            u, v, w = edge
-            if selected_deg[u] < k and selected_deg[v] < k:
-                ans += w
-                selected_deg[u] += 1
-                selected_deg[v] += 1
-        return ans
+        adj = [[] for _ in range(n)]
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            adj[v].append((u, w))
+        
+        def dfs(u, p):
+            gains = []
+            total_full = 0
+            for v, w in adj[u]:
+                if v == p:
+                    continue
+                f, r = dfs(v, u)
+                total_full += f
+                gains.append(w + r - f)
+            gains.sort(reverse=True)
+            gain_max_full = 0
+            gain_max_red = 0
+            current = 0
+            for i in range(len(gains)):
+                current += gains[i]
+                s = i + 1
+                if s <= k:
+                    gain_max_full = max(gain_max_full, current)
+                if s <= k - 1:
+                    gain_max_red = max(gain_max_red, current)
+            full = total_full + gain_max_full
+            red = total_full + gain_max_red
+            return full, red
+        
+        return dfs(0, -1)[0]
 # @lc code=end
