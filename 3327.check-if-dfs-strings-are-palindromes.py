eb4bd8433f83174f2333f7cc4aa6533749fea802
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:20:47 +0800

    Batch add 1 solutions: 3327.check-if-dfs-st...

--- 3327.check-if-dfs-strings-are-palindromes.py
+++ 3327.check-if-dfs-strings-are-palindromes.py
@@ -5,62 +5,68 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def findAnswer(self, parent: List[int], s: str) -> List[bool]:
         n = len(parent)
-        adj = [[] for _ in range(n)]
+        # Build adjacency list
+        tree = [[] for _ in range(n)]
         for i in range(1, n):
-            adj[parent[i]].append(i)
-        for u in range(n):
-            adj[u].sort()
-        
-        MOD1 = 1000000007
-        MOD2 = 1000000009
-        BASE1 = 131
-        BASE2 = 137
-        
-        pow1 = [1] * (n + 1)
-        pow2 = [1] * (n + 1)
-        for i in range(1, n + 1):
-            pow1[i] = pow1[i - 1] * BASE1 % MOD1
-            pow2[i] = pow2[i - 1] * BASE2 % MOD2
-        
-        h_fwd = [[0, 0] for _ in range(n)]
-        h_rev = [[0, 0] for _ in range(n)]
-        sz = [0] * n
-        
-        def dfs(u):
-            children = adj[u]
-            total_sz = 1
-            hf1, hf2 = 0, 0
-            for c in children:
-                dfs(c)
-                total_sz += sz[c]
-                p1 = pow1[sz[c]]
-                p2 = pow2[sz[c]]
-                hf1 = (hf1 * p1 + h_fwd[c][0]) % MOD1
-                hf2 = (hf2 * p2 + h_fwd[c][1]) % MOD2
-            ch = ord(s[u]) - ord('a') + 1
-            p1 = pow1[1]
-            p2 = pow2[1]
-            hf1 = (hf1 * p1 + ch) % MOD1
-            hf2 = (hf2 * p2 + ch) % MOD2
-            h_fwd[u] = [hf1, hf2]
-            sz[u] = total_sz
-            
-            hr1 = ch % MOD1
-            hr2 = ch % MOD2
-            for c in reversed(children):
-                p1 = pow1[sz[c]]
-                p2 = pow2[sz[c]]
-                hr1 = (hr1 * p1 + h_rev[c][0]) % MOD1
-                hr2 = (hr2 * p2 + h_rev[c][1]) % MOD2
-            h_rev[u] = [hr1, hr2]
-        
-        import sys
-        sys.setrecursionlimit(100010)
+            tree[parent[i]].append(i)
+        for node in range(n):
+            tree[node].sort()  # Ensure increasing order traversal
+
+        # Hash parameters
+        MOD1, MOD2 = 10**9 + 7, 10**9 + 9
+        BASE1, BASE2 = 911, 3571
+
+        # Precompute powers for both hashes
+        pow1 = [1] * (n + 2)
+        pow2 = [1] * (n + 2)
+        for i in range(1, n + 2):
+            pow1[i] = (pow1[i-1] * BASE1) % MOD1
+            pow2[i] = (pow2[i-1] * BASE2) % MOD2
+
+        # Store hashes and lengths for each node
+        fw_hash1 = [0] * n
+        fw_hash2 = [0] * n
+        rv_hash1 = [0] * n
+        rv_hash2 = [0] * n
+        length = [0] * n
+
+        def dfs(x):
+            h1, h2 = 0, 0
+            rh1, rh2 = 0, 0
+            l = 0
+            for y in tree[x]:
+                dfs(y)
+                # Append child's hashes/lengths to current
+                h1 = (h1 * pow1[length[y]] + fw_hash1[y]) % MOD1
+                h2 = (h2 * pow2[length[y]] + fw_hash2[y]) % MOD2
+                rh1 = (rv_hash1[y] * pow1[l] + rh1) % MOD1
+                rh2 = (rv_hash2[y] * pow2[l] + rh2) % MOD2
+                l += length[y]
+            # Add current node's character
+            code = ord(s[x]) - ord('a') + 1
+            h1 = (h1 * BASE1 + code) % MOD1
+            h2 = (h2 * BASE2 + code) % MOD2
+            rh1 = (rh1 + code * pow1[l]) % MOD1
+            rh2 = (rh2 + code * pow2[l]) % MOD2
+            l += 1
+            fw_hash1[x], fw_hash2[x] = h1, h2
+            rv_hash1[x], rv_hash2[x] = rh1, rh2
+            length[x] = l
+
+        for i in range(n):
+            length[i] = 0
         dfs(0)
-        
-        answer = [(h_fwd[i][0] == h_rev[i][0] and h_fwd[i][1] == h_rev[i][1]) for i in range(n)]
+
+        answer = [False] * n
+        def dfs_check(x):
+            # For the subtree rooted at x, check palindrome
+            answer[x] = (fw_hash1[x] == rv_hash1[x]) and (fw_hash2[x] == rv_hash2[x])
+            for y in tree[x]:
+                dfs_check(y)
+        dfs_check(0)
         return answer
 # @lc code=end
