cc9ef2773dc259dde612ea000abc4f2a061919d6
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 16:05:37 +0800

    Batch add 1 solutions: 3513.number-of-uniqu...

---
+++ 3513.number-of-unique-xor-triplets-i.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=3513 lang=python3
+#
+# [3513] Number of Unique XOR Triplets I
+#
+
+# @lc code=start
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return n
+        return 1 << n.bit_length()
+# @lc code=end
