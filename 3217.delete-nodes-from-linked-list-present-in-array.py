72bfbc5abb03c1b7d3137e941561522857205f43
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:18:34 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.py
+++ 3217.delete-nodes-from-linked-list-present-in-array.py
@@ -9,16 +9,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import List, Optional
+
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        num_set = set(nums)
+        nums_set = set(nums)
         dummy = ListNode(0)
         dummy.next = head
-        prev = dummy
-        while prev.next:
-            if prev.next.val in num_set:
-                prev.next = prev.next.next
+        prev, curr = dummy, head
+        while curr:
+            if curr.val in nums_set:
+                prev.next = curr.next
             else:
-                prev = prev.next
+                prev = curr
+            curr = curr.next
         return dummy.next
 # @lc code=end
