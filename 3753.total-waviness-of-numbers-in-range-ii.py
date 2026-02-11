a52b2024c9d5276cca8cbe8a6a39d88ca2ce47f3
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:22:32 +0800

    Batch add 1 solutions: 3753.total-waviness-...

--- 3753.total-waviness-of-numbers-in-range-ii.py
+++ 3753.total-waviness-of-numbers-in-range-ii.py
@@ -7,49 +7,43 @@
 # @lc code=start
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def compute(num):
-            if num < 1:
-                return 0
-            S = str(num)
-            L = len(S)
-            memo = {}
-            def dp(pos, tight, pp, p, alen):
-                if pos == L:
-                    return 1, 0
-                key = (pos, tight, pp, p, alen)
-                if key in memo:
-                    return memo[key]
-                res_cnt = 0
-                res_sum = 0
-                up = 9 if tight == 0 else int(S[pos])
-                for d in range(up + 1):
-                    ntight = 1 if tight == 1 and d == up else 0
-                    if alen == 0:
-                        if d == 0:
-                            nal = 0
-                            npp = 10
-                            np = 10
-                            contrib = 0
-                        else:
-                            nal = 1
-                            npp = 10
-                            np = d
-                            contrib = 0
+        from functools import lru_cache
+        def waviness_up_to(n: int) -> int:
+            digits = list(map(int, str(n)))
+            length = len(digits)
+            @lru_cache(maxsize=None)
+            def dp(pos, tight, window, leading_zero, sig_count):
+                if pos == length:
+                    return 0
+                res = 0
+                up = digits[pos] if tight else 9
+                for d in range(0, up+1):
+                    new_leading_zero = leading_zero and d == 0
+                    new_sig_count = sig_count if new_leading_zero else sig_count + 1
+                    new_tight = tight and (d == up)
+                    # Update window with new significant digit
+                    if new_leading_zero:
+                        new_window = window
                     else:
-                        contrib = 0
-                        if alen >= 2:
-                            if p > pp and p > d:
-                                contrib += 1
-                            if p < pp and p < d:
-                                contrib += 1
-                        nal = alen + 1
-                        npp = p
-                        np = d
-                    cnt, suma = dp(pos + 1, ntight, npp, np, nal)
-                    res_cnt += cnt
-                    res_sum += suma + contrib * cnt
-                memo[key] = (res_cnt, res_sum)
-                return memo[key]
-            return dp(0, 1, 10, 10, 0)[1]
-        return compute(num2) - compute(num1 - 1)
+                        new_window = window + (d,)
+                        if len(new_window) > 3:
+                            new_window = new_window[-3:]
+                    # Evaluate waviness only when window has 3 digits and not at the edges
+                    if not new_leading_zero and len(new_window) == 3:
+                        # Only count if this digit is not the first or last significant digit
+                        if new_sig_count >= 3 and pos < length-1:
+                            a, b, c = new_window
+                            is_peak = b > a and b > c
+                            is_valley = b < a and b < c
+                            waviness = 1 if (is_peak or is_valley) else 0
+                        else:
+                            waviness = 0
+                        res += waviness + dp(pos+1, new_tight, new_window, new_leading_zero, new_sig_count)
+                    else:
+                        res += dp(pos+1, new_tight, new_window, new_leading_zero, new_sig_count)
+                return res
+            return dp(0, True, tuple(), True, 0)
+        def safe_minus_one(x):
+            return max(x-1, 0)
+        return waviness_up_to(num2) - waviness_up_to(safe_minus_one(num1))
 # @lc code=end
