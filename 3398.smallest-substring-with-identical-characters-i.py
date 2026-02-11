2979d6bfe42154a4854c2f4baf73e3f940a3c3d6
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 13:09:44 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.py
+++ 3398.smallest-substring-with-identical-characters-i.py
@@ -3,32 +3,37 @@
 #
 # [3398] Smallest Substring With Identical Characters I
 #
+
 # @lc code=start
 class Solution:
     def minLength(self, s: str, numOps: int) -> int:
         n = len(s)
         def can(L: int) -> bool:
             INF = n + 1
-            prev_dp = [[INF] * 2 for _ in range(L + 1)]
+            prev = [[INF] * (L + 1) for _ in range(2)]
             for c in range(2):
-                cost = (str(c) != s[0])
-                prev_dp[1][c] = cost
-            for pos in range(1, n):
-                curr_dp = [[INF] * 2 for _ in range(L + 1)]
-                for r in range(1, L + 1):
-                    for pc in range(2):
-                        if prev_dp[r][pc] == INF:
+                cost = 0 if int(s[0]) == c else 1
+                prev[c][1] = cost
+            for i in range(1, n):
+                curr = [[INF] * (L + 1) for _ in range(2)]
+                for pc in range(2):
+                    for pk in range(1, L + 1):
+                        if prev[pc][pk] == INF:
                             continue
                         for nc in range(2):
-                            cost = (str(nc) != s[pos])
-                            nr = r + 1 if nc == pc else 1
-                            if nr > L:
-                                continue
-                            curr_dp[nr][nc] = min(curr_dp[nr][nc], prev_dp[r][pc] + cost)
-                prev_dp = curr_dp
-            min_flips = min(min(prev_dp[r][0], prev_dp[r][1]) for r in range(1, L + 1))
-            return min_flips <= numOps
-        
+                            cost = 0 if int(s[i]) == nc else 1
+                            if nc == pc and pk + 1 <= L:
+                                nk = pk + 1
+                                curr[nc][nk] = min(curr[nc][nk], prev[pc][pk] + cost)
+                            elif nc != pc:
+                                nk = 1
+                                curr[nc][nk] = min(curr[nc][nk], prev[pc][pk] + cost)
+                prev = curr
+            minf = INF
+            for c in range(2):
+                for k in range(1, L + 1):
+                    minf = min(minf, prev[c][k])
+            return minf <= numOps
         left, right = 1, n
         while left < right:
             mid = (left + right) // 2
