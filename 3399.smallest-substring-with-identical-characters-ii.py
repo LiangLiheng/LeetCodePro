d3eee59e61c3b036c1fd0a579e80012f4b129020
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-11 13:30:43 +0800

    Batch add 1 solutions: 3399.smallest-substr...

--- 3399.smallest-substring-with-identical-characters-ii.py
+++ 3399.smallest-substring-with-identical-characters-ii.py
@@ -3,37 +3,31 @@
 #
 # [3399] Smallest Substring With Identical Characters II
 #
-
 # @lc code=start
 class Solution:
     def minLength(self, s: str, numOps: int) -> int:
         n = len(s)
-        def flips_needed(k: int) -> int:
-            def simulate(start: int) -> int:
-                cost = 0 if int(s[0]) == start else 1
-                cur = start
-                run = 1
-                for i in range(1, n):
-                    nxt = int(s[i])
-                    if nxt == cur and run == k:
-                        cost += 1
-                        cur = 1 - cur
-                        run = 1
-                    elif nxt == cur:
-                        run += 1
-                    else:
-                        cur = nxt
-                        run = 1
-                return cost
-            return min(simulate(0), simulate(1))
-        left = 1
-        right = n
+        
+        def can_achieve(k: int) -> bool:
+            total_flips = 0
+            i = 0
+            while i < n:
+                j = i
+                while j < n and s[j] == s[i]:
+                    j += 1
+                length = j - i
+                total_flips += length // (k + 1)
+                if total_flips > numOps:
+                    return False
+                i = j
+            return total_flips <= numOps
+        
+        left, right = 1, n
         while left < right:
-            mid = left + (right - left) // 2
-            if flips_needed(mid) <= numOps:
+            mid = (left + right) // 2
+            if can_achieve(mid):
                 right = mid
             else:
                 left = mid + 1
         return left
-
 # @lc code=end
