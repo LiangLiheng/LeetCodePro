1ce893e16df3bf9757012865b01330e92d2aaeaa
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 04:05:36 +0800

    Batch add 1 solutions: 3493.properties-grap...

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -8,33 +8,31 @@
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
-        node_sets = [set(p) for p in properties]
-        parent = list(range(n))
-        rank = [0] * n
-        
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-        
-        def union(x, y):
-            px, py = find(x), find(y)
-            if px == py:
-                return
-            if rank[px] < rank[py]:
-                parent[px] = py
-            elif rank[px] > rank[py]:
-                parent[py] = px
-            else:
-                parent[py] = px
-                rank[px] += 1
-        
+        # Build adjacency list
+        adj = [[] for _ in range(n)]
+        def intersect(a, b):
+            return len(set(a) & set(b))
         for i in range(n):
-            for j in range(i + 1, n):
-                if len(node_sets[i] & node_sets[j]) >= k:
-                    union(i, j)
-        
-        components = set(find(i) for i in range(n))
-        return len(components)
-
+            for j in range(i+1, n):
+                if intersect(properties[i], properties[j]) >= k:
+                    adj[i].append(j)
+                    adj[j].append(i)
+        # Verify adjacency: each edge is undirected and intersection condition holds
+        # (Omitted in code for efficiency, but should be considered in reasoning)
+        visited = [False] * n
+        def dfs(u):
+            stack = [u]
+            while stack:
+                node = stack.pop()
+                for v in adj[node]:
+                    if not visited[v]:
+                        visited[v] = True
+                        stack.append(v)
+        components = 0
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                dfs(i)
+                components += 1
+        return components
 # @lc code=end
