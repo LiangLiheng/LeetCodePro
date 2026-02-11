17513a1965bc965e951f178c6f1bf8a202c87724
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:03:00 +0800

    Batch add 1 solutions: 3686.number-of-stabl...

--- 3686.number-of-stable-subsequences.py
+++ 3686.number-of-stable-subsequences.py
@@ -5,26 +5,27 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
-        prev = [[0] * 2 for _ in range(2)]
+        # dp1: subsequences ending with 1 consecutive parity (even/odd)
+        # dp2: subsequences ending with 2 consecutive parity (even/odd)
+        dp1 = [0, 0]  # [even, odd]
+        dp2 = [0, 0]  # [even, odd]
         for num in nums:
-            cur_p = num % 2
-            curr = [[0] * 2 for _ in range(2)]
-            for c in range(2):
-                for p in range(2):
-                    curr[c][p] = prev[c][p]
-            curr[0][cur_p] = (curr[0][cur_p] + 1) % MOD
-            sum_diff = (prev[0][1 - cur_p] + prev[1][1 - cur_p]) % MOD
-            curr[0][cur_p] = (curr[0][cur_p] + sum_diff) % MOD
-            sum_same = prev[0][cur_p]
-            curr[1][cur_p] = (curr[1][cur_p] + sum_same) % MOD
-            prev = curr
-        total = 0
-        for c in range(2):
-            for p in range(2):
-                total = (total + prev[c][p]) % MOD
-        return total
-
+            p = num % 2
+            q = 1 - p
+            new_dp1 = dp1.copy()
+            new_dp2 = dp2.copy()
+            # If we continue with same parity (and only 1 so far):
+            new_dp2[p] = (new_dp2[p] + dp1[p]) % MOD
+            # If we switch parity, can start new streak of 1
+            new_dp1[p] = (new_dp1[p] + dp1[q] + dp2[q]) % MOD
+            # Can always start with this element as a new subsequence
+            new_dp1[p] = (new_dp1[p] + 1) % MOD
+            dp1, dp2 = new_dp1, new_dp2
+        res = (dp1[0] + dp1[1] + dp2[0] + dp2[1] - 1) % MOD
+        return res
 # @lc code=end
