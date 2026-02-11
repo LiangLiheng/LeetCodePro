79f3e3a1205b98f410b593b62767851616465b97
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:15:48 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.py
+++ 3739.count-subarrays-with-majority-element-ii.py
@@ -5,29 +5,26 @@
 #
 
 # @lc code=start
+from typing import List
+from collections import defaultdict
+import bisect
+
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        offset = n + 1
-        MAXV = 2 * n + 5
-        ft = [0] * (MAXV + 1)
-        def update(idx):
-            while idx <= MAXV:
-                ft[idx] += 1
-                idx += idx & -idx
-        def query(idx):
-            res = 0
-            while idx > 0:
-                res += ft[idx]
-                idx -= idx & -idx
-            return res
-        ans = 0
-        prefix = 0
-        update(offset + prefix)
-        for num in nums:
-            prefix += 1 if num == target else -1
-            ans += query(offset + prefix - 1)
-            update(offset + prefix)
-        return ans
-
+        # Transform nums into +1 (target) and -1 (not target)
+        arr = [1 if num == target else -1 for num in nums]
+        prefix = [0]
+        for v in arr:
+            prefix.append(prefix[-1] + v)
+        # We want to count number of pairs (i, j) with i < j where prefix[j] > prefix[i]
+        # Use a sorted list to maintain prefix sums
+        from bisect import bisect_left, insort
+        res = 0
+        sl = [0]
+        for i in range(1, len(prefix)):
+            # Count number of prefix sums less than prefix[i]
+            pos = bisect_left(sl, prefix[i])
+            res += pos
+            insort(sl, prefix[i])
+        return res
 # @lc code=end
