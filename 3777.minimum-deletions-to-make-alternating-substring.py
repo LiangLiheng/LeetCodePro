d44ffed3faa4730ff88a69ba56be11f60bd9c0c6
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 05:30:48 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.py
+++ 3777.minimum-deletions-to-make-alternating-substring.py
@@ -5,64 +5,61 @@
 #
 
 # @lc code=start
-from typing import List
-
 class Solution:
     def minDeletions(self, s: str, queries: List[List[int]]) -> List[int]:
-        class FenwickTree:
-            def __init__(self, size: int):
-                self.size = size
-                self.tree = [0] * (size + 2)
-
-            def update(self, index: int, delta: int):
-                index += 1
-                while index <= self.size:
-                    self.tree[index] += delta
-                    index += index & -index
-
-            def query(self, index: int) -> int:
-                index += 1
-                res = 0
-                while index > 0:
-                    res += self.tree[index]
-                    index -= index & -index
-                return res
-
-            def range_query(self, left: int, right: int) -> int:
-                return self.query(right) - (self.query(left - 1) if left > 0 else 0)
-
         n = len(s)
-        s_list = list(s)
+        s = list(s)
+        class SegmentTree:
+            def __init__(self, n):
+                self.N = n - 1
+                self.size = 1
+                while self.size < self.N:
+                    self.size <<= 1
+                self.tree = [0] * (self.size * 2)
+            def build(self, data):
+                for i in range(self.N):
+                    self.tree[self.size + i] = data[i]
+                for i in range(self.size - 1, 0, -1):
+                    self.tree[i] = self.tree[i << 1] + self.tree[i << 1 | 1]
+            def update(self, idx, val):
+                idx += self.size
+                self.tree[idx] = val
+                while idx > 1:
+                    idx >>= 1
+                    self.tree[idx] = self.tree[idx << 1] + self.tree[idx << 1 | 1]
+            def query(self, l, r):
+                res = 0
+                l += self.size
+                r += self.size + 1
+                while l < r:
+                    if l & 1:
+                        res += self.tree[l]
+                        l += 1
+                    if r & 1:
+                        r -= 1
+                        res += self.tree[r]
+                    l >>= 1
+                    r >>= 1
+                return res
+        # Build initial data: 1 if s[i] == s[i+1], else 0
+        data = [1 if s[i] == s[i+1] else 0 for i in range(n-1)]
+        seg = SegmentTree(n)
+        seg.build(data)
         ans = []
-        ft = FenwickTree(n - 1) if n > 1 else None
-        if n > 1:
-            for i in range(n - 1):
-                val = 1 if s_list[i] != s_list[i + 1] else 0
-                ft.update(i, val)
-        for qry in queries:
-            if len(qry) == 2:
-                # flip
-                j = qry[1]
-                old_left = 1 if j > 0 and s_list[j - 1] != s_list[j] else 0
-                old_right = 1 if j < n - 1 and s_list[j] != s_list[j + 1] else 0
-                s_list[j] = 'A' if s_list[j] == 'B' else 'B'
-                new_left = 1 if j > 0 and s_list[j - 1] != s_list[j] else 0
-                new_right = 1 if j < n - 1 and s_list[j] != s_list[j + 1] else 0
-                if ft is not None:
-                    if j > 0:
-                        ft.update(j - 1, new_left - old_left)
-                    if j < n - 1:
-                        ft.update(j, new_right - old_right)
+        for q in queries:
+            if q[0] == 1:
+                j = q[1]
+                # flip s[j]
+                s[j] = 'A' if s[j] == 'B' else 'B'
+                if j > 0:
+                    seg.update(j-1, 1 if s[j-1] == s[j] else 0)
+                if j < n-1:
+                    seg.update(j, 1 if s[j] == s[j+1] else 0)
             else:
-                # query
-                l, r = qry[1], qry[2]
-                sublen = r - l + 1
-                if sublen == 1:
+                l, r = q[1], q[2]
+                if l == r:
                     ans.append(0)
                 else:
-                    changes = ft.range_query(l, r - 1)
-                    dels = sublen - 1 - changes
-                    ans.append(dels)
+                    ans.append(seg.query(l, r-1))
         return ans
-
 # @lc code=end
