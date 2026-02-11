439950ba19177b1b0d020967f71ca8f39ca74c64
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:16:42 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -5,41 +5,29 @@
 #
 
 # @lc code=start
-from collections import deque
-
 class TextEditor:
-
     def __init__(self):
-        self.left = []
-        self.right = deque()
+        self.left = []  # characters to the left of the cursor
+        self.right = []  # characters to the right of the cursor
 
     def addText(self, text: str) -> None:
         for c in text:
             self.left.append(c)
 
     def deleteText(self, k: int) -> int:
-        deleted = min(k, len(self.left))
-        for _ in range(deleted):
+        cnt = 0
+        while self.left and cnt < k:
             self.left.pop()
-        return deleted
+            cnt += 1
+        return cnt
 
     def cursorLeft(self, k: int) -> str:
-        moves = min(k, len(self.left))
-        for _ in range(moves):
-            self.right.appendleft(self.left.pop())
+        for _ in range(min(k, len(self.left))):
+            self.right.append(self.left.pop())
         return ''.join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        moves = min(k, len(self.right))
-        for _ in range(moves):
-            self.left.append(self.right.popleft())
+        for _ in range(min(k, len(self.right))):
+            self.left.append(self.right.pop())
         return ''.join(self.left[-10:])
-
-
-# Your TextEditor object will be instantiated and called as such:
-# obj = TextEditor()
-# obj.addText(text)
-# param_2 = obj.deleteText(k)
-# param_3 = obj.cursorLeft(k)
-# param_4 = obj.cursorRight(k)
 # @lc code=end
