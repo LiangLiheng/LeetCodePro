964e2cb78066e999d4d3ef75028504223f00a448
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 04:15:42 +0800

    Batch add 1 solutions: 3515.shortest-path-i...

--- 3515.shortest-path-in-a-weighted-tree.py
+++ 3515.shortest-path-in-a-weighted-tree.py
@@ -1,1 +1,81 @@
-#\n# @lc app=leetcode id=3515 lang=python3\n#\n# [3515] Shortest Path in a Weighted Tree\n#\n\n# @lc code=start\nfrom typing import List\nimport sys\nsys.setrecursionlimit(10**6)\n\nclass FenwickTree:\n    def __init__(self, size: int):\n        self.size = size\n        self.tree = [0] * (size + 1)\n\n    def update(self, index: int, delta: int) -> None:\n        while index <= self.size:\n            self.tree[index] += delta\n            index += index & -index\n\n    def query(self, index: int) -> int:\n        res = 0\n        while index > 0:\n            res += self.tree[index]\n            index -= index & -index\n        return res\n\nclass Solution:\n    def treeQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:\n        adj = [[] for _ in range(n + 1)]\n        for a, b, c in edges:\n            adj[a].append((b, c))\n            adj[b].append((a, c))\n        parent = [0] * (n + 1)\n        edge_weight = [0] * (n + 1)\n        in_time = [0] * (n + 1)\n        out_time = [0] * (n + 1)\n        timer = [0]\n        def dfs(node: int, par: int) -> None:\n            parent[node] = par\n            timer[0] += 1\n            in_time[node] = timer[0]\n            for nei, w in adj[node]:\n                if nei != par:\n                    edge_weight[nei] = w\n                    dfs(nei, node)\n            timer[0] += 1\n            out_time[node] = timer[0]\n        dfs(1, 0)\n        ft = FenwickTree(timer[0])\n        for i in range(2, n + 1):\n            ft.update(in_time[i], edge_weight[i])\n            ft.update(out_time[i], -edge_weight[i])\n        ans = []\n        for query in queries:\n            if query[0] == 2:\n                ans.append(ft.query(in_time[query[1]]))\n            else:\n                u, v, w = query[1], query[2], query[3]\n                child = u if parent[u] == v else v\n                delta = w - edge_weight[child]\n                edge_weight[child] = w\n                ft.update(in_time[child], delta)\n                ft.update(out_time[child], -delta)\n        return ans\n\n# @lc code=end
+#
+# @lc app=leetcode id=3515 lang=python3
+#
+# [3515] Shortest Path in a Weighted Tree
+#
+
+# @lc code=start
+class Solution:
+    def treeQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        from collections import defaultdict
+        
+        tree = defaultdict(list)
+        for u, v, w in edges:
+            tree[u].append((v, w))
+            tree[v].append((u, w))
+        
+        parent = [0] * (n + 1)
+        depth = [0] * (n + 1)
+        dist = [0] * (n + 1)
+        def dfs(u, p):
+            for v, w in tree[u]:
+                if v == p:
+                    continue
+                parent[v] = u
+                depth[v] = depth[u] + 1
+                dist[v] = dist[u] + w
+                dfs(v, u)
+        dfs(1, 0)
+        
+        in_time = [0] * (n + 1)
+        out_time = [0] * (n + 1)
+        time = [0]
+        def euler(u, p):
+            time[0] += 1
+            in_time[u] = time[0]
+            for v, _ in tree[u]:
+                if v == p:
+                    continue
+                euler(v, u)
+            out_time[u] = time[0]
+        euler(1, 0)
+        
+        class BIT:
+            def __init__(self, size):
+                self.N = size + 2
+                self.tree = [0] * (self.N)
+            def update(self, x, delta):
+                while x < self.N:
+                    self.tree[x] += delta
+                    x += (x & -x)
+            def query(self, x):
+                res = 0
+                while x > 0:
+                    res += self.tree[x]
+                    x -= (x & -x)
+                return res
+            def range_add(self, l, r, delta):
+                self.update(l, delta)
+                self.update(r + 1, -delta)
+        bit = BIT(n + 2)
+        edge_weights = {}
+        for u, v, w in edges:
+            edge_weights[(u, v)] = edge_weights[(v, u)] = w
+        ans = []
+        for q in queries:
+            if q[0] == 1:
+                _, u, v, w_new = q
+                if depth[u] > depth[v]:
+                    child = u
+                else:
+                    child = v
+                w_old = edge_weights[(u, v)]
+                delta = w_new - w_old
+                bit.range_add(in_time[child], out_time[child], delta)
+                edge_weights[(u, v)] = edge_weights[(v, u)] = w_new
+            elif q[0] == 2:
+                _, x = q
+                res = dist[x] + bit.query(in_time[x])
+                ans.append(res)
+        return ans
+# @lc code=end
