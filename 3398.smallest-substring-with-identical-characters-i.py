5bd5b8efae1a95db9f83ed3135ccb770317769af
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 03:28:36 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.py
+++ 3398.smallest-substring-with-identical-characters-i.py
@@ -5,42 +5,30 @@
 #
 
 # @lc code=start
+import math
 class Solution:
     def minLength(self, s: str, numOps: int) -> int:
         n = len(s)
-        def can(L: int) -> bool:
-            INF = n + 1
-            prev = [[INF] * (L + 1) for _ in range(2)]
-            for c in range(2):
-                cost = 0 if int(s[0]) == c else 1
-                prev[c][1] = cost
-            for i in range(1, n):
-                curr = [[INF] * (L + 1) for _ in range(2)]
-                for pc in range(2):
-                    for pk in range(1, L + 1):
-                        if prev[pc][pk] == INF:
-                            continue
-                        for nc in range(2):
-                            cost = 0 if int(s[i]) == nc else 1
-                            if nc == pc and pk + 1 <= L:
-                                nk = pk + 1
-                                curr[nc][nk] = min(curr[nc][nk], prev[pc][pk] + cost)
-                            elif nc != pc:
-                                nk = 1
-                                curr[nc][nk] = min(curr[nc][nk], prev[pc][pk] + cost)
-                prev = curr
-            minf = INF
-            for c in range(2):
-                for k in range(1, L + 1):
-                    minf = min(minf, prev[c][k])
-            return minf <= numOps
+        def can_make(L):
+            flips_needed = 0
+            i = 0
+            while i < n:
+                j = i
+                while j < n and s[j] == s[i]:
+                    j += 1
+                run_len = j - i
+                if run_len > L:
+                    flips_needed += math.ceil(run_len / L) - 1
+                i = j
+            return flips_needed <= numOps
         left, right = 1, n
-        while left < right:
+        answer = n
+        while left <= right:
             mid = (left + right) // 2
-            if can(mid):
-                right = mid
+            if can_make(mid):
+                answer = mid
+                right = mid - 1
             else:
                 left = mid + 1
-        return left
-
+        return answer
 # @lc code=end
