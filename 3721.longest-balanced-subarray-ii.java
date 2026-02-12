2ab350a828b3a1d56a37a76e42d09b6a3145df75
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:17:49 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.java
+++ 3721.longest-balanced-subarray-ii.java
@@ -5,48 +5,26 @@
 #
 
 # @lc code=start
+import java.util.*;
 class Solution {
-  public int longestBalanced(int[] nums) {
-    int n = nums.length;
-    final int MAX = 100001;
-    int[] firstPos = new int[MAX];
-    int[] lastPos = new int[MAX];
-    java.util.Arrays.fill(firstPos, -1);
-    java.util.Arrays.fill(lastPos, -1);
-    for (int i = 0; i < n; i++) {
-      int val = nums[i];
-      if (firstPos[val] == -1) {
-        firstPos[val] = i;
-      }
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int maxLen = 0;
+        for (int left = 0; left < n; ++left) {
+            Set<Integer> evenSet = new HashSet<>();
+            Set<Integer> oddSet = new HashSet<>();
+            for (int right = left; right < n; ++right) {
+                if (nums[right] % 2 == 0) {
+                    evenSet.add(nums[right]);
+                } else {
+                    oddSet.add(nums[right]);
+                }
+                if (evenSet.size() == oddSet.size()) {
+                    maxLen = Math.max(maxLen, right - left + 1);
+                }
+            }
+        }
+        return maxLen;
     }
-    for (int i = n - 1; i >= 0; i--) {
-      int val = nums[i];
-      if (lastPos[val] == -1) {
-        lastPos[val] = i;
-      }
-    }
-    java.util.HashMap<Integer, Integer> minIdx = new java.util.HashMap<>();
-    minIdx.put(0, -1);
-    int pd = 0;
-    int fd = 0;
-    int ans = 0;
-    for (int r = 0; r < n; r++) {
-      int val = nums[r];
-      if (firstPos[val] == r) {
-        int sign = (val % 2 == 0 ? 1 : -1);
-        pd += sign;
-      }
-      if (minIdx.containsKey(pd)) {
-        int idx = minIdx.get(pd);
-        ans = Math.max(ans, r - idx);
-      }
-      if (lastPos[val] == r) {
-        int sign = (val % 2 == 0 ? 1 : -1);
-        fd += sign;
-        minIdx.putIfAbsent(fd, r);
-      }
-    }
-    return ans;
-  }
 }
 # @lc code=end
