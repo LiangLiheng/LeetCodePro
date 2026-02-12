8f5a09018642ea9ab091a6c954a108fa312a3326
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 20:08:26 +0800

    Batch add 1 solutions: 3513.number-of-uniqu...

--- 3513.number-of-unique-xor-triplets-i.java
+++ 3513.number-of-unique-xor-triplets-i.java
@@ -3,16 +3,24 @@
 #
 # [3513] Number of Unique XOR Triplets I
 #
-
 # @lc code=start
+import java.util.*;
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-        if (n <= 2) {
-            return n;
+        Set<Integer> xorSet = new HashSet<>();
+        int[] prefixXor = new int[n + 1];
+        prefixXor[0] = 0;
+        for (int i = 0; i < n; i++) {
+            prefixXor[i + 1] = prefixXor[i] ^ nums[i];
         }
-        int lz = Integer.numberOfLeadingZeros(n);
-        return 1 << (32 - lz);
+        // Enumerate all subarrays to get all possible triplet XORs efficiently
+        for (int i = 0; i < n; i++) {
+            for (int k = i; k < n; k++) {
+                xorSet.add(prefixXor[k + 1] ^ prefixXor[i]);
+            }
+        }
+        return xorSet.size();
     }
 }
 # @lc code=end
