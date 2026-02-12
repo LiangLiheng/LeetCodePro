4903ae3867f1661394fa4e7c48a3b5d0f492c156
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 19:18:53 +0800

    Batch add 1 solutions: 3420.count-non-decre...

---
+++ 3420.count-non-decreasing-subarrays-after-k-operations.java
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=3420 lang=java
+#
+# [3420] Count Non-Decreasing Subarrays After K Operations
+#
+# @lc code=start
+class Solution {
+    public long countNonDecreasingSubarrays(int[] nums, int k) {
+        // Implement the approach as described in the reasoning above
+    }
+}
+# @lc code=end
