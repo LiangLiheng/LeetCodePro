29837945c605a5476e3978b9e1c67df5b3c26284
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:16:02 +0800

    Batch add 1 solutions: 3704.count-no-zero-p...

--- 3704.count-no-zero-pairs-that-sum-to-n.java
+++ 3704.count-no-zero-pairs-that-sum-to-n.java
@@ -3,54 +3,50 @@
 #
 # [3704] Count No-Zero Pairs That Sum to N
 #
-
 # @lc code=start
 class Solution {
-  public long countNoZeroPairs(long n) {
-    String s = String.valueOf(n);
-    int L = s.length();
-    int[] digits = new int[L];
-    for (int i = 0; i < L; ++i) {
-      digits[i] = s.charAt(i) - '0';
+    public long countNoZeroPairs(long n) {
+        int[] digits = getDigits(n);
+        // dp[pos][carry][tight]: position, carry, tightness with n
+        Long[][][] memo = new Long[digits.length + 1][2][2];
+        return dfs(0, 0, 1, digits, memo);
     }
-    long[][][][] dp = new long[L + 1][2][2][2];
-    for (int sa = 0; sa < 2; ++sa) {
-      for (int sb = 0; sb < 2; ++sb) {
-        dp[L][sa][sb][0] = (sa == 1 && sb == 1) ? 1L : 0L;
-        dp[L][sa][sb][1] = 0L;
-      }
-    }
-    for (int pos = L - 1; pos >= 0; --pos) {
-      for (int sa = 0; sa < 2; ++sa) {
-        for (int sb = 0; sb < 2; ++sb) {
-          long tot0 = 0L;
-          long tot1 = 0L;
-          int mina = sa == 1 ? 1 : 0;
-          int minb = sb == 1 ? 1 : 0;
-          for (int da = mina; da <= 9; ++da) {
-            int nsa = sa | (da != 0 ? 1 : 0);
-            for (int db = minb; db <= 9; ++db) {
-              int nsb = sb | (db != 0 ? 1 : 0);
-              for (int cin = 0; cin < 2; ++cin) {
-                int sumv = da + db + cin;
-                if (sumv % 10 == digits[pos]) {
-                  int cout = sumv / 10;
-                  long ways = dp[pos + 1][nsa][nsb][cin];
-                  if (cout == 0) {
-                    tot0 += ways;
-                  } else {
-                    tot1 += ways;
-                  }
-                }
-              }
-            }
-          }
-          dp[pos][sa][sb][0] = tot0;
-          dp[pos][sa][sb][1] = tot1;
+
+    // Extract digits from n, least to most significant (right to left)
+    private int[] getDigits(long n) {
+        String s = Long.toString(n);
+        int[] res = new int[s.length()];
+        for (int i = s.length() - 1, j = 0; i >= 0; --i, ++j) {
+            res[j] = s.charAt(i) - '0';
         }
-      }
+        return res;
     }
-    return dp[0][0][0][0];
-  }
+
+    // pos: digit position (0 = least significant)
+    // carry: carry from previous sum
+    // tight: if true, sum so far equals n's prefix
+    private long dfs(int pos, int carry, int tight, int[] digits, Long[][][] memo) {
+        if (pos == digits.length) {
+            return carry == 0 ? 1 : 0;
+        }
+        if (memo[pos][carry][tight] != null) return memo[pos][carry][tight];
+        long res = 0;
+        int nDigit = digits[pos];
+        int maxSum = tight == 1 ? nDigit : 18;
+        for (int a = 1; a <= 9; ++a) {
+            for (int b = 1; b <= 9; ++b) {
+                int sum = a + b + carry;
+                int curDigit = sum % 10;
+                int newCarry = sum / 10;
+                if (curDigit > maxSum) continue;
+                if (tight == 1 && curDigit < nDigit) continue;
+                if (tight == 1 && curDigit > nDigit) continue;
+                int nextTight = (tight == 1 && curDigit == nDigit) ? 1 : 0;
+                res += dfs(pos + 1, newCarry, nextTight, digits, memo);
+            }
+        }
+        memo[pos][carry][tight] = res;
+        return res;
+    }
 }
 # @lc code=end
