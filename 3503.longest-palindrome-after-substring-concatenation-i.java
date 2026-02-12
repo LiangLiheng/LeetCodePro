70699b733eff638ca486bd404870ca6e6da553ed
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 19:57:53 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.java
+++ 3503.longest-palindrome-after-substring-concatenation-i.java
@@ -3,55 +3,35 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
+
 # @lc code=start
 class Solution {
-    private boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-        while (left < right) {
-            if (str.charAt(left++) != str.charAt(right--)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public int longestPalindrome(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        int ans = 0;
-        // s only palindromes (empty t)
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                if (isPalindrome(s.substring(i, j))) {
-                    ans = Math.max(ans, j - i);
-                }
-            }
-        }
-        // t only palindromes (empty s)
-        for (int i = 0; i < m; ++i) {
-            for (int j = i + 1; j <= m; ++j) {
-                if (isPalindrome(t.substring(i, j))) {
-                    ans = Math.max(ans, j - i);
-                }
-            }
-        }
-        // both non-empty
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                String leftPart = s.substring(i, j);
-                for (int p = 0; p < m; ++p) {
-                    for (int q = p + 1; q <= m; ++q) {
-                        String rightPart = t.substring(p, q);
-                        String combined = leftPart + rightPart;
-                        if (isPalindrome(combined)) {
-                            ans = Math.max(ans, combined.length());
+        int maxLen = 0;
+        int n = s.length(), m = t.length();
+        for (int i = 0; i <= n; ++i) {
+            for (int j = i; j <= n; ++j) {
+                String ss = s.substring(i, j);
+                for (int k = 0; k <= m; ++k) {
+                    for (int l = k; l <= m; ++l) {
+                        String tt = t.substring(k, l);
+                        String combined = ss + tt;
+                        if (combined.length() > 0 && isPalindrome(combined)) {
+                            maxLen = Math.max(maxLen, combined.length());
                         }
                     }
                 }
             }
         }
-        return ans;
+        return maxLen;
+    }
+    private boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++; right--;
+        }
+        return true;
     }
 }
 # @lc code=end
