455681e315be273045c6b19c415453826f8b4189
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:10:22 +0800

    Batch add 1 solutions: 3686.number-of-stabl...

--- 3686.number-of-stable-subsequences.java
+++ 3686.number-of-stable-subsequences.java
@@ -3,44 +3,33 @@
 #
 # [3686] Number of Stable Subsequences
 #
-
 # @lc code=start
 class Solution {
     public int countStableSubsequences(int[] nums) {
-        long MOD = 1000000007L;
-        long dp10 = 0L, dp20 = 0L, dp11 = 0L, dp21 = 0L;
+        final int MOD = 1000000007;
+        int n = nums.length;
+        // dp[parity][consecutive]: number of subsequences ending with given parity and number of consecutive
+        long[][] dp = new long[2][3]; // [0]=even, [1]=odd, [1,2] consecutive
         for (int num : nums) {
-            int cur = num % 2;
-            long ndp10 = dp10;
-            long ndp20 = dp20;
-            long ndp11 = dp11;
-            long ndp21 = dp21;
-            // new single
-            if (cur == 0) {
-                ndp10 = (ndp10 + 1) % MOD;
-            } else {
-                ndp11 = (ndp11 + 1) % MOD;
-            }
-            // append to diff parity
-            long sumDiff = (cur == 0 ? (dp11 + dp21) % MOD : (dp10 + dp20) % MOD);
-            if (cur == 0) {
-                ndp10 = (ndp10 + sumDiff) % MOD;
-            } else {
-                ndp11 = (ndp11 + sumDiff) % MOD;
-            }
-            // append to same streak 1
-            long same1 = (cur == 0 ? dp10 : dp11);
-            if (cur == 0) {
-                ndp20 = (ndp20 + same1) % MOD;
-            } else {
-                ndp21 = (ndp21 + same1) % MOD;
-            }
-            dp10 = ndp10;
-            dp20 = ndp20;
-            dp11 = ndp11;
-            dp21 = ndp21;
+            int p = num % 2;
+            int q = 1 - p;
+            long[] newDp0 = new long[3];
+            long[] newDp1 = new long[3];
+            // Start a new subsequence with this number
+            if (p == 0) newDp0[1] = 1;
+            else newDp1[1] = 1;
+            // Extend previous subsequences
+            // If previous ends with different parity, can append and k=1
+            newDp0[1] = (newDp0[1] + dp[1][1] + dp[1][2]) % MOD;
+            newDp1[1] = (newDp1[1] + dp[0][1] + dp[0][2]) % MOD;
+            // If previous ends with same parity and k=1, can extend to k=2
+            newDp0[2] = (newDp0[2] + dp[0][1]) % MOD;
+            newDp1[2] = (newDp1[2] + dp[1][1]) % MOD;
+            dp[0] = newDp0;
+            dp[1] = newDp1;
         }
-        return (int) ((dp10 + dp20 + dp11 + dp21) % MOD);
+        long total = (dp[0][1] + dp[0][2] + dp[1][1] + dp[1][2]) % MOD;
+        return (int)total;
     }
 }
 # @lc code=end
