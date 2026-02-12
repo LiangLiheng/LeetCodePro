b7ff19b27834171cb175ba0b3c6b2f43100a270c
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 20:19:28 +0800

    Batch add 1 solutions: 3519.count-numbers-w...

--- 3519.count-numbers-with-non-decreasing-digits.java
+++ 3519.count-numbers-with-non-decreasing-digits.java
@@ -1,67 +1,86 @@
-import java.math.BigInteger;
 #
 # @lc app=leetcode id=3519 lang=java
 #
 # [3519] Count Numbers with Non-Decreasing Digits
 #
+
 # @lc code=start
 class Solution {
-    private static final long MOD = 1000000007L;
-
+    static final int MOD = 1000000007;
+    
     public int countNumbers(String l, String r, int b) {
-        long countR = solve(r, b);
-        BigInteger Lbig = new BigInteger(l);
-        long countL = 0L;
-        if (Lbig.compareTo(BigInteger.ZERO) > 0) {
-            String lMinusOneStr = Lbig.subtract(BigInteger.ONE).toString();
-            countL = solve(lMinusOneStr, b);
-        }
-        long result = (countR - countL + MOD) % MOD;
-        return (int) result;
+        int[] rDigits = toBaseB(r, b);
+        int[] lDigits = toBaseB(l, b);
+        int countR = countNonDecreasing(rDigits, b);
+        int[] lMinus1 = subtractOne(lDigits, b);
+        int countL = countNonDecreasing(lMinus1, b);
+        int ans = (countR - countL + MOD) % MOD;
+        return ans;
     }
-
-    private long solve(String S, int b) {
-        BigInteger num = new BigInteger(S);
-        String baseStr = num.toString(b);
-        int L = baseStr.length();
-        int[] digits = new int[L];
-        for (int i = 0; i < L; i++) {
-            digits[i] = baseStr.charAt(i) - '0';
+    
+    // Converts a decimal string to a digit array in base b (most significant digit first)
+    private int[] toBaseB(String s, int b) {
+        java.util.ArrayList<Integer> digits = new java.util.ArrayList<>();
+        java.math.BigInteger base = java.math.BigInteger.valueOf(b);
+        java.math.BigInteger num = new java.math.BigInteger(s);
+        if (num.equals(java.math.BigInteger.ZERO)) return new int[]{0};
+        while (num.compareTo(java.math.BigInteger.ZERO) > 0) {
+            digits.add(0, num.mod(base).intValue());
+            num = num.divide(base);
         }
-        long[][][][] dp = new long[L + 1][b][2][2];
-        for (int p = 0; p < b; p++) {
-            for (int t = 0; t < 2; t++) {
-                for (int z = 0; z < 2; z++) {
-                    dp[L][p][t][z] = 1L;
-                }
+        int[] arr = new int[digits.size()];
+        for (int i = 0; i < digits.size(); ++i) arr[i] = digits.get(i);
+        return arr;
+    }
+    
+    // Subtract one from a base-b digit array
+    private int[] subtractOne(int[] digits, int b) {
+        int n = digits.length;
+        int[] res = digits.clone();
+        int i = n - 1;
+        while (i >= 0) {
+            if (res[i] > 0) {
+                res[i]--;
+                break;
+            } else {
+                res[i] = b - 1;
+                i--;
             }
         }
-        for (int pos = L - 1; pos >= 0; pos--) {
-            for (int prev = 0; prev < b; prev++) {
-                for (int tight = 0; tight < 2; tight++) {
-                    for (int leadz = 0; leadz < 2; leadz++) {
-                        long res = 0L;
-                        int up = tight == 1 ? digits[pos] : b - 1;
-                        for (int d = 0; d <= up; d++) {
-                            int newTight = (tight == 1 && d == up) ? 1 : 0;
-                            int newLeadz = (leadz == 1 && d == 0) ? 1 : 0;
-                            int newPrev;
-                            if (newLeadz == 1) {
-                                newPrev = 0;
-                            } else if (leadz == 1) {
-                                newPrev = d;
-                            } else {
-                                if (d < prev) continue;
-                                newPrev = d;
-                            }
-                            res = (res + dp[pos + 1][newPrev][newTight][newLeadz]) % MOD;
-                        }
-                        dp[pos][prev][tight][leadz] = res;
-                    }
-                }
-            }
+        // Remove leading zeros
+        int firstNonZero = 0;
+        while (firstNonZero < res.length - 1 && res[firstNonZero] == 0) firstNonZero++;
+        int[] trimmed = new int[res.length - firstNonZero];
+        System.arraycopy(res, firstNonZero, trimmed, 0, trimmed.length);
+        return trimmed;
+    }
+    
+    // Digit DP to count numbers with non-decreasing digits up to given digit array
+    private int countNonDecreasing(int[] digits, int b) {
+        int n = digits.length;
+        Integer[][][][] dp = new Integer[n + 1][b][2][2];
+        return dfs(0, 0, true, true, digits, b, dp);
+    }
+    // pos: current digit position
+    // prev: previous digit selected (for leading zeros, 0 is default)
+    // tight: whether prefix matches bound
+    // leadingZero: whether leading zeros are still present
+    private int dfs(int pos, int prev, boolean tight, boolean leadingZero, int[] digits, int b, Integer[][][][] dp) {
+        if (pos == digits.length) return leadingZero ? 0 : 1;
+        int tightInt = tight ? 1 : 0;
+        int lzInt = leadingZero ? 1 : 0;
+        if (dp[pos][prev][tightInt][lzInt] != null) return dp[pos][prev][tightInt][lzInt];
+        int res = 0;
+        int up = tight ? digits[pos] : b - 1;
+        for (int d = 0; d <= up; ++d) {
+            if (!leadingZero && d < prev) continue;
+            boolean nextTight = tight && (d == up);
+            boolean nextLZ = leadingZero && (d == 0);
+            int nextPrev = (leadingZero && d == 0) ? 0 : d;
+            res = (res + dfs(pos + 1, nextPrev, nextTight, nextLZ, digits, b, dp)) % MOD;
         }
-        return dp[0][0][1][1];
+        dp[pos][prev][tightInt][lzInt] = res;
+        return res;
     }
 }
 # @lc code=end
