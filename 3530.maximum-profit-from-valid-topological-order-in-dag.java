1c9690a8188dba8fe0c7e3be82ac97a6bdfae159
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 20:14:44 +0800

    Batch add 1 solutions: 3530.maximum-profit-...

--- 3530.maximum-profit-from-valid-topological-order-in-dag.java
+++ 3530.maximum-profit-from-valid-topological-order-in-dag.java
@@ -3,31 +3,26 @@
 #
 # [3530] Maximum Profit from Valid Topological Order in DAG
 #
-
 # @lc code=start
 class Solution {
-  public int maxProfit(int n, int[][] edges, int[] score) {
-    int[] predmask = new int[n];
-    for (int[] edge : edges) {
-      predmask[edge[1]] |= (1 << edge[0]);
-    }
-    int N = 1 << n;
-    int[] dp = new int[N];
-    for (int i = 0; i < N; i++) {
-      dp[i] = Integer.MIN_VALUE / 2;
-    }
-    dp[0] = 0;
-    for (int mask = 0; mask < N; mask++) {
-      if (dp[mask] < 0) continue;
-      int pos = Integer.bitCount(mask) + 1;
-      for (int u = 0; u < n; u++) {
-        if (((mask & (1 << u)) == 0) && ((predmask[u] & mask) == predmask[u])) {
-          int nmask = mask | (1 << u);
-          dp[nmask] = Math.max(dp[nmask], dp[mask] + score[u] * pos);
+    public int maxProfit(int n, int[][] edges, int[] score) {
+        int[] deps = new int[n]; // dependencies as bitmask
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            deps[v] |= (1 << u);
         }
-      }
+        int N = 1 << n;
+        int[] dp = new int[N];
+        for (int mask = 0; mask < N; ++mask) {
+            int pos = Integer.bitCount(mask); // number of processed nodes
+            for (int u = 0; u < n; ++u) {
+                if ((mask & (1 << u)) == 0 && (deps[u] & ~mask) == 0) {
+                    int newMask = mask | (1 << u);
+                    dp[newMask] = Math.max(dp[newMask], dp[mask] + score[u] * (pos + 1));
+                }
+            }
+        }
+        return dp[N-1];
     }
-    return dp[N - 1];
-  }
 }
 # @lc code=end
