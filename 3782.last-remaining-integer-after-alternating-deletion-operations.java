3b59d7780be7092753b25386dfd62e2334a3eb78
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:42:20 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.java
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.java
@@ -3,31 +3,21 @@
 #
 # [3782] Last Remaining Integer After Alternating Deletion Operations
 #
-
 # @lc code=start
 class Solution {
     public long lastInteger(long n) {
-        return fLeft(n);
-    }
-
-    private long fLeft(long n) {
-        if (n == 1) {
-            return 1;
+        long head = 1;
+        long step = 1;
+        boolean left = true;
+        while (n > 1) {
+            if (left || n % 2 == 1) {
+                head += step;
+            }
+            n /= 2;
+            step *= 2;
+            left = !left;
         }
-        return 2 * fRight((n + 1) / 2) - 1;
-    }
-
-    private long fRight(long n) {
-        if (n == 1) {
-            return 1;
-        }
-        long m = (n + 1) / 2;
-        long fl = fLeft(m);
-        if (n % 2 == 0) {
-            return 2 * fl;
-        } else {
-            return 2 * fl - 1;
-        }
+        return head;
     }
 }
 # @lc code=end
