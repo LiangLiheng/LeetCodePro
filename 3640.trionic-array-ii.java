99899a900d073bb2d87799b8b78753f685d352e9
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:01:22 +0800

    Batch add 1 solutions: 3640.trionic-array-i...

--- 3640.trionic-array-ii.java
+++ 3640.trionic-array-ii.java
@@ -3,63 +3,48 @@
 #
 # [3640] Trionic Array II
 #
-
 # @lc code=start
 class Solution {
     public long maxSumTrionic(int[] nums) {
         int n = nums.length;
         long[] prefix = new long[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
         }
-        int[] left_inc = new int[n];
-        long[] left_min_pref = new long[n];
-        left_inc[0] = 0;
-        left_min_pref[0] = Long.MAX_VALUE / 2;
+        int[] incLeft = new int[n]; // length of strictly increasing sequence ending at i
+        int[] dec = new int[n];     // length of strictly decreasing sequence ending at i
+        int[] incRight = new int[n]; // length of strictly increasing sequence starting at i
+        incLeft[0] = 1;
         for (int i = 1; i < n; ++i) {
-            if (nums[i - 1] < nums[i]) {
-                left_inc[i] = left_inc[i - 1];
-                left_min_pref[i] = Math.min(left_min_pref[i - 1], prefix[i - 1]);
-            } else {
-                left_inc[i] = i;
-                left_min_pref[i] = Long.MAX_VALUE / 2;
-            }
+            incLeft[i] = (nums[i - 1] < nums[i]) ? incLeft[i - 1] + 1 : 1;
         }
-        int[] right_inc = new int[n];
-        long[] right_max_pref = new long[n];
-        right_inc[n - 1] = n - 1;
-        right_max_pref[n - 1] = Long.MIN_VALUE / 2;
+        dec[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            dec[i] = (nums[i - 1] > nums[i]) ? dec[i - 1] + 1 : 1;
+        }
+        incRight[n - 1] = 1;
         for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] < nums[i + 1]) {
-                right_inc[i] = right_inc[i + 1];
-                right_max_pref[i] = Math.max(prefix[i + 2], right_max_pref[i + 1]);
-            } else {
-                right_inc[i] = i;
-                right_max_pref[i] = Long.MIN_VALUE / 2;
-            }
+            incRight[i] = (nums[i] < nums[i + 1]) ? incRight[i + 1] + 1 : 1;
         }
-        int[] right_dec = new int[n];
-        right_dec[n - 1] = n - 1;
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] > nums[i + 1]) {
-                right_dec[i] = right_dec[i + 1];
-            } else {
-                right_dec[i] = i;
-            }
+        long maxSum = Long.MIN_VALUE;
+        // For each possible middle segment (the strictly decreasing part)
+        for (int mid = 1; mid < n - 1; ++mid) {
+            int decLen = dec[mid];
+            if (decLen < 2) continue; // strictly decreasing needs at least 2 elements
+            int q = mid - decLen + 1; // start of decreasing segment
+            int p = q - 1; // end of left increasing segment
+            if (p < 0 || incLeft[p] < 2) continue; // left increasing must exist and have at least 2 elements
+            int l = p - incLeft[p] + 1; // start of left increasing
+            int r = mid + 1; // start of right increasing segment
+            if (r >= n || incRight[r] < 2) continue; // right increasing must exist and have at least 2 elements
+            int rr = r + incRight[r] - 1; // end of right increasing
+            // Verify segments are non-overlapping, contiguous, and within bounds
+            if (l < 0 || rr >= n || p + 1 != q || mid + 1 != r) continue;
+            // Optionally, you could add explicit checks to ensure each segment is strictly monotonic if unsure
+            long sum = prefix[rr + 1] - prefix[l];
+            maxSum = Math.max(maxSum, sum);
         }
-        long ans = Long.MIN_VALUE;
-        for (int p = 1; p < n - 1; ++p) {
-            if (nums[p - 1] < nums[p] && nums[p] > nums[p + 1]) {
-                int q = right_dec[p];
-                if (q < n - 1 && nums[q] < nums[q + 1]) {
-                    long cur = right_max_pref[q] - left_min_pref[p];
-                    if (cur > ans) {
-                        ans = cur;
-                    }
-                }
-            }
-        }
-        return ans;
+        return maxSum;
     }
 }
 # @lc code=end
