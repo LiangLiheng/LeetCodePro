1e15a23f74004e5088447d9f97963f05a6355da4
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:07:18 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.java
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.java
@@ -3,36 +3,23 @@
 #
 # [3670] Maximum Product of Two Integers With No Common Bits
 #
-
 # @lc code=start
 class Solution {
     public long maxProduct(int[] nums) {
-        final int BITS = 20;
-        final int MAXM = 1 << BITS;
-        final int MASK = MAXM - 1;
-        boolean[] has = new boolean[MAXM];
+        Map<Integer, Integer> maskToMax = new HashMap<>();
         for (int x : nums) {
-            if (x < MAXM) {
-                has[x] = true;
-            }
-        }
-        long[] dp = new long[MAXM];
-        for (int i = 0; i < MAXM; i++) {
-            dp[i] = has[i] ? i : 0L;
-        }
-        for (int b = 0; b < BITS; b++) {
-            for (int m = 0; m < MAXM; m++) {
-                if ((m & (1 << b)) == 0) {
-                    int sup = m | (1 << b);
-                    dp[sup] = Math.max(dp[sup], dp[m]);
-                }
-            }
+            int mask = x;
+            maskToMax.put(mask, Math.max(maskToMax.getOrDefault(mask, 0), x));
         }
         long ans = 0;
-        for (int w : nums) {
-            int comp = (~w) & MASK;
-            long v = dp[comp];
-            ans = Math.max(ans, v * (long) w);
+        Integer[] masks = maskToMax.keySet().toArray(new Integer[0]);
+        for (int i = 0; i < masks.length; i++) {
+            for (int j = i + 1; j < masks.length; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    long prod = 1L * maskToMax.get(masks[i]) * maskToMax.get(masks[j]);
+                    if (prod > ans) ans = prod;
+                }
+            }
         }
         return ans;
     }
