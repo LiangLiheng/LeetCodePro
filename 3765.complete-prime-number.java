4900859f5e2bd4d07a92bc87b8de031d488ef30b
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:34:33 +0800

    Batch add 1 solutions: 3765.complete-prime-...

--- 3765.complete-prime-number.java
+++ 3765.complete-prime-number.java
@@ -1,45 +1,29 @@
-#
-# @lc app=leetcode id=3765 lang=java
-#
-# [3765] Complete Prime Number
-#
-
-# @lc code=start
+// @lc app=leetcode id=3765 lang=java
+//
+// [3765] Complete Prime Number
+//
+// @lc code=start
 class Solution {
     public boolean completePrime(int num) {
         String s = String.valueOf(num);
-        int len = s.length();
-        for (int i = 1; i <= len; ++i) {
-            long prefix = Long.parseLong(s.substring(0, i));
-            if (!isPrime(prefix)) {
-                return false;
-            }
-        }
-        for (int i = 1; i <= len; ++i) {
-            long suffix = Long.parseLong(s.substring(len - i));
-            if (!isPrime(suffix)) {
+        int n = s.length();
+        for (int i = 1; i <= n; ++i) {
+            int prefix = Integer.parseInt(s.substring(0, i));
+            int suffix = Integer.parseInt(s.substring(n - i));
+            if (!isPrime(prefix) || !isPrime(suffix)) {
                 return false;
             }
         }
         return true;
     }
-
-    private boolean isPrime(long n) {
-        if (n <= 1) {
-            return false;
-        }
-        if (n <= 3) {
-            return true;
-        }
-        if (n % 2 == 0 || n % 3 == 0) {
-            return false;
-        }
-        for (long i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+        for (int i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) return false;
         }
         return true;
     }
 }
-# @lc code=end
+// @lc code=end
