f82c066ba309e87285d9244e773874beca29d0e4
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 21:38:16 +0800

    Batch add 1 solutions: 3771.total-score-of-...

--- 3771.total-score-of-dungeon-runs.java
+++ 3771.total-score-of-dungeon-runs.java
@@ -1,1 +1,26 @@
-#\n# @lc app=leetcode id=3771 lang=java\n#\n# [3771] Total Score of Dungeon Runs\n#\n\n# @lc code=start\nclass Solution {\n    public long totalScore(int hp, int[] damage, int[] requirement) {\n        int n = damage.length;\n        long[] prefix = new long[n + 1];\n        for (int i = 1; i <= n; i++) {\n            prefix[i] = prefix[i - 1] + damage[i - 1];\n        }\n        long ans = 0;\n        for (int k = 0; k < n; k++) {\n            long thresh = (long) requirement[k] + prefix[k + 1] - hp;\n            int left = 0, right = k;\n            int pos = k + 1;\n            while (left <= right) {\n                int mid = left + (right - left) / 2;\n                if (prefix[mid] >= thresh) {\n                    pos = mid;\n                    right = mid - 1;\n                } else {\n                    left = mid + 1;\n                }\n            }\n            if (pos <= k) {\n                ans += (k - pos + 1L);\n            }\n        }\n        return ans;\n    }\n}\n# @lc code=end
+# @lc app=leetcode id=3771 lang=java
+#
+# [3771] Total Score of Dungeon Runs
+#
+# @lc code=start
+class Solution {
+    public long totalScore(int hp, int[] damage, int[] requirement) {
+        int n = damage.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + damage[i];
+        }
+        long total = 0;
+        for (int start = 0; start < n; ++start) {
+            long curHp = hp;
+            for (int i = start; i < n; ++i) {
+                curHp -= damage[i];
+                if (curHp >= requirement[i]) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+}
+# @lc code=end
