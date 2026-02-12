dc0e7a074d85a33f8848e87bad4caff3b1748355
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 20:13:21 +0800

    Batch add 1 solutions: 3525.find-x-value-of...

--- 3525.find-x-value-of-array-ii.java
+++ 3525.find-x-value-of-array-ii.java
@@ -3,107 +3,44 @@
 #
 # [3525] Find X Value of Array II
 #
-
 # @lc code=start
 class Solution {
-    static class Node {
-        int[] cnt;
-        int total;
-        Node(int k) {
-            cnt = new int[k];
-            total = 0;
-        }
-    }
-
-    private Node[] tree;
-    private int KK;
-
-    private void merge(Node parent, Node left, Node right) {
-        parent.total = (int) ((long) left.total * right.total % KK);
-        System.arraycopy(left.cnt, 0, parent.cnt, 0, KK);
-        int[] temp = new int[KK];
-        for (int rem = 0; rem < KK; rem++) {
-            int comb = (int) ((long) left.total * rem % KK);
-            temp[comb] += right.cnt[rem];
-        }
-        for (int i = 0; i < KK; i++) {
-            parent.cnt[i] += temp[i];
-        }
-    }
-
-    private Node getNeutral() {
-        Node neu = new Node(KK);
-        neu.total = 1;
-        return neu;
-    }
-
-    private void build(int node, int start, int end, int[] mods) {
-        if (start == end) {
-            tree[node] = new Node(KK);
-            tree[node].cnt[mods[start]] = 1;
-            tree[node].total = mods[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        build(2 * node, start, mid, mods);
-        build(2 * node + 1, mid + 1, end, mods);
-        tree[node] = new Node(KK);
-        merge(tree[node], tree[2 * node], tree[2 * node + 1]);
-    }
-
-    private void update(int node, int start, int end, int idx, int val) {
-        if (start == end) {
-            tree[node] = new Node(KK);
-            tree[node].cnt[val] = 1;
-            tree[node].total = val;
-            return;
-        }
-        int mid = (start + end) / 2;
-        if (idx <= mid) {
-            update(2 * node, start, mid, idx, val);
-        } else {
-            update(2 * node + 1, mid + 1, end, idx, val);
-        }
-        tree[node] = new Node(KK);
-        merge(tree[node], tree[2 * node], tree[2 * node + 1]);
-    }
-
-    private Node query(int node, int start, int end, int l, int r) {
-        if (l > end || r < start) {
-            return getNeutral();
-        }
-        if (l <= start && end <= r) {
-            return tree[node];
-        }
-        int mid = (start + end) / 2;
-        Node left = query(2 * node, start, mid, l, r);
-        Node right = query(2 * node + 1, mid + 1, end, l, r);
-        Node res = new Node(KK);
-        merge(res, left, right);
-        return res;
-    }
-
     public int[] resultArray(int[] nums, int k, int[][] queries) {
         int n = nums.length;
-        KK = k;
-        int[] mods = new int[n];
-        for (int i = 0; i < n; i++) {
-            mods[i] = (int) ((long) nums[i] % k);
+        int q = queries.length;
+        int[] res = new int[q];
+        for (int queryIdx = 0; queryIdx < q; ++queryIdx) {
+            int index = queries[queryIdx][0];
+            int value = queries[queryIdx][1];
+            int start = queries[queryIdx][2];
+            int xi = queries[queryIdx][3];
+            // Verify valid index and update
+            if (index >= 0 && index < n) {
+                nums[index] = value;
+            }
+            // Remove prefix
+            int m = n - start;
+            if (m <= 0) {
+                res[queryIdx] = 0;
+                continue;
+            }
+            int[] arr = new int[m];
+            for (int i = 0; i < m; ++i) arr[i] = nums[start + i];
+            int[] dp = new int[k];
+            for (int i = m - 1; i >= 0; --i) {
+                int[] ndp = new int[k];
+                int mod = arr[i] % k;
+                ndp[mod] += 1; // Single element suffix
+                for (int r = 0; r < k; ++r) {
+                    int newR = (r * mod) % k;
+                    ndp[newR] += dp[r];
+                }
+                dp = ndp;
+            }
+            // Double-check: dp[xi] should count all non-empty suffixes with product mod k == xi
+            res[queryIdx] = dp[xi];
         }
-        tree = new Node[4 * n + 10];
-        build(1, 0, n - 1, mods);
-        int[] ans = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int idx = queries[i][0];
-            int val = queries[i][1];
-            int sti = queries[i][2];
-            int xi = queries[i][3];
-            int newm = (int) ((long) val % k);
-            update(1, 0, n - 1, idx, newm);
-            Node res = query(1, 0, n - 1, sti, n - 1);
-            ans[i] = res.cnt[xi];
-        }
-        return ans;
+        return res;
     }
 }
 # @lc code=end
