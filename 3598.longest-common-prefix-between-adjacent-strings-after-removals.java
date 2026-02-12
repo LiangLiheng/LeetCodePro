f71bb6a0fc2f00c5a21d67aa0129845e621f8adf
LiangLiheng <LiangLiheng@users.noreply.github.com>
2026-02-12 20:45:38 +0800

    Batch add 1 solutions: 3598.longest-common-...

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.java
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.java
@@ -3,50 +3,36 @@
 #
 # [3598] Longest Common Prefix Between Adjacent Strings After Removals
 #
-
 # @lc code=start
 class Solution {
-    private static int getLcp(String a, String b) {
-        int minLen = Math.min(a.length(), b.length());
-        for (int k = 0; k < minLen; k++) {
-            if (a.charAt(k) != b.charAt(k)) {
-                return k;
-            }
-        }
-        return minLen;
+    private int getLCP(String a, String b) {
+        int len = Math.min(a.length(), b.length());
+        int i = 0;
+        while (i < len && a.charAt(i) == b.charAt(i)) i++;
+        return i;
     }
-
     public int[] longestCommonPrefix(String[] words) {
         int n = words.length;
-        int[] ans = new int[n];
-        if (n < 2) {
-            return ans;
+        if (n == 1) return new int[]{0};
+        int[] lcp = new int[n-1];
+        for (int i = 0; i < n-1; i++) {
+            lcp[i] = getLCP(words[i], words[i+1]);
         }
-        int[] pre = new int[n - 1];
-        for (int j = 0; j < n - 1; j++) {
-            pre[j] = getLcp(words[j], words[j + 1]);
-        }
-        int[] leftMax = new int[n];
-        leftMax[0] = 0;
-        for (int j = 1; j < n; j++) {
-            leftMax[j] = Math.max(leftMax[j - 1], pre[j - 1]);
-        }
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = 0;
-        for (int k = n - 2; k >= 0; k--) {
-            rightMax[k] = Math.max(rightMax[k + 1], pre[k]);
-        }
+        int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
-            int cand1 = (i >= 2) ? leftMax[i - 1] : 0;
-            int cand2 = (i <= n - 3) ? rightMax[i + 1] : 0;
-            int origMax = Math.max(cand1, cand2);
-            int bridge = 0;
-            if (i > 0 && i < n - 1) {
-                bridge = getLcp(words[i - 1], words[i + 1]);
+            int maxLCP = 0;
+            // If i-1 and i+1 are valid, compute new LCP
+            if (i-1 >= 0 && i+1 < n) {
+                maxLCP = getLCP(words[i-1], words[i+1]);
             }
-            ans[i] = Math.max(origMax, bridge);
+            // Consider unaffected LCPs
+            for (int j = 0; j < n-1; j++) {
+                if (j == i-1 || j == i) continue; // affected pairs
+                maxLCP = Math.max(maxLCP, lcp[j]);
+            }
+            answer[i] = maxLCP;
         }
-        return ans;
+        return answer;
     }
 }
 # @lc code=end
